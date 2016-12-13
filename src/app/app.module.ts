258c8b049bd3323f483ac8a8f8bb68df50d3ce3f
Pascal Precht <pascal.precht@googlemail.com>
2016-12-13 16:48:23 +0100

    chore: remove unused FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { ApiService } from './api.service';
@@ -12,7 +11,6 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpModule
   ],
   providers: [ApiService],
