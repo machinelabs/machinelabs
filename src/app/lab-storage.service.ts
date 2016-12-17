e1da060f316b15f109ea6f4ebf51471d1c9d63a0
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2016-12-17 23:57:04 +0100

    chore: move blueprint code in seperate file

--- src/app/lab-storage.service.ts
+++ src/app/lab-storage.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs/Observable';
 import * as shortid from 'shortid';
 
 import { Lab } from './models/lab';
+import { DEFAULT_LAB_CODE } from './default-lab';
 
 declare let firebase: any;
 
@@ -35,27 +36,7 @@ export class LabStorageService {
       id: shortid.generate(),
       // TODO: we may wanna change the return type to Observable<Lab> and prefill with userId
       userId: '',
-      code: `import numpy as np
-from keras.models import Sequential
-from keras.layers.core import Dense
-
-# the four different states of the XOR gate
-training_data = np.array([[0,0],[0,1],[1,0],[1,1]], "float32")
-
-# the four expected results in the same order
-target_data = np.array([[0],[1],[1],[0]], "float32")
-
-model = Sequential()
-model.add(Dense(16, input_dim=2, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-
-model.compile(loss='mean_squared_error',
-              optimizer='adam',
-              metrics=['binary_accuracy'])
-
-model.fit(training_data, target_data, nb_epoch=500, verbose=2)
-
-print model.predict(training_data).round()`
+      code: DEFAULT_LAB_CODE
     };
   }
 
