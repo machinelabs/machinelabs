f41d46a9ebaef6d5acb5f1623b9cbf7207c616a8
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2016-12-24 14:09:56 +0100

    fix: prevent cursor from jumping
    Fixes #8

--- src/app/ace-editor/ace-editor.component.ts
+++ src/app/ace-editor/ace-editor.component.ts
@@ -53,7 +53,9 @@ export class AceEditorComponent implements AfterViewInit {
     // we need to check if editor is defined because ngOnChanges
     // runs before ngAfterViewInit(). Is there a better way?
     if (this.editor !== undefined) {
-      if (changes.value && this.value !== null) {
+      if (changes.value 
+          && this.value !== null
+          && this.value !== this.editor.getValue()) {
         this.editor.setValue(this.value, 1);
       }
 
