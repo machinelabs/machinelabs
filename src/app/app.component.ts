8f8be59fdb68a322a991c52b602362db52f36a79
Pascal Precht <pascal.precht@googlemail.com>
2016-12-26 16:39:59 +0100

    chore: remove unused scss file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ml-app',
-  template: '<router-outlet></router-outlet>',
-  styleUrls: ['./app.component.scss']
+  template: '<router-outlet></router-outlet>'
 })
 export class AppComponent {}
