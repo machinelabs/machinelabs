6b4e10b859182830abd2d29b9f2297f9d70eb329
Pascal Precht <pascal.precht@googlemail.com>
2017-03-06 20:11:28 +0100

    chore: add test environment

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  offline: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDegzorhSxWcXV7Nl1bf-mIjGRgGb5dCCY",
+    authDomain: "machinelabs-test.firebaseapp.com",
+    databaseURL: "https://machinelabs-test.firebaseio.com",
+    storageBucket: "machinelabs-test.appspot.com",
+    messagingSenderId: "219672978749"
+  }
+};
+
