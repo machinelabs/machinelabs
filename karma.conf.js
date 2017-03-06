a56017ffc7e99c60b096e5e1a957cf3911e18ca2
Pascal Precht <pascal.precht@googlemail.com>
2017-03-06 20:19:37 +0100

    chore: use mocha reporter instead of progress reporter

--- karma.conf.js
+++ karma.conf.js
@@ -32,8 +32,8 @@ module.exports = function (config) {
       environment: 'dev'
     },
     reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['progress', 'karma-remap-istanbul']
-              : ['progress'],
+              ? ['mocha', 'karma-remap-istanbul']
+              : ['mocha'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
