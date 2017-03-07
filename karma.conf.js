f1a436622e28949b0f47053b8bf37f46403d750f
Pascal Precht <pascal.precht@googlemail.com>
2017-03-07 20:33:42 +0100

    chore: add travis CI support

--- karma.conf.js
+++ karma.conf.js
@@ -39,6 +39,16 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    customLaunchers: {
+      Chrome_travis_ci: {
+          base: 'Chrome',
+          flags: ['--no-sandbox']
+      }
+    }
   });
+
+  if (process.env.TRAVIS) {
+    config.browsers = ['Chrome_travis_ci'];
+  }
 };
