2a0619c4537291a672501cebaf84ba9437bc551d
Pascal Precht <pascal.precht@googlemail.com>
2017-03-10 15:43:37 +0100

    tests(PanelComponent): adds missing tests

--- src/app/panel/panel.component.spec.ts
+++ src/app/panel/panel.component.spec.ts
@@ -1,7 +1,5 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 
 import { PanelComponent } from './panel.component';
 
@@ -9,20 +7,22 @@ describe('PanelComponent', () => {
   let component: PanelComponent;
   let fixture: ComponentFixture<PanelComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ PanelComponent ]
-    })
-    .compileComponents();
-  }));
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PanelComponent]
+    });
+
     fixture = TestBed.createComponent(PanelComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render panel title', () => {
+    component.panelTitle = 'Panel title';
+
+    let titleElement = fixture.debugElement.query(By.css('h2')).nativeElement;
+
+    fixture.detectChanges();
+    expect(titleElement.textContent).toContain('Panel title');
   });
 });
