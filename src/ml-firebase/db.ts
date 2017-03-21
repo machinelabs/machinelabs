3594e7866ca1bd28dd2657ee3843bb737b08abe1
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-03-21 13:10:50 +0100

    fix: use envirnomnent in ml-firebase

--- src/ml-firebase/db.ts
+++ src/ml-firebase/db.ts
@@ -1,10 +1,4 @@
 import * as firebase from 'firebase';
 import { environment } from '../environments/environment';
 
-export const db = this.db = firebase.initializeApp({
-      apiKey: "AIzaSyDu0Qds2fWo8iZMcCj0T_ANqD9V4E0_9QY",
-      authDomain: "machinelabs-a73cd.firebaseapp.com",
-      databaseURL: "https://machinelabs-a73cd.firebaseio.com",
-      storageBucket: "machinelabs-a73cd.appspot.com",
-      messagingSenderId: "351438476852"
-    }).database();
+export const db = this.db = firebase.initializeApp(environment.firebaseConfig).database();
