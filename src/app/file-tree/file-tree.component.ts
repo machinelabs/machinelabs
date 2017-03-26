929bc5ae3611647cad7b1d7a3db4deea0230da5a
Pascal Precht <pascal.precht@googlemail.com>
2017-03-26 20:15:59 +0100

    feat(FileTreeComponent): add editFile Ouput and and DOM

--- src/app/file-tree/file-tree.component.ts
+++ src/app/file-tree/file-tree.component.ts
@@ -5,23 +5,7 @@ const MAIN_PYTHON_FILENAME = 'main.py';
 
 @Component({
   selector: 'ml-file-tree',
-  template: `
-    <ul class="ml-file-list">
-      <li
-        class="ml-file-list__item"
-        *ngFor="let file of files"
-        (click)="selectFile.next(file)"
-        [ngClass]="{ selected: selectedFile && selectedFile.name == file.name }">
-
-        <md-icon>description</md-icon> {{file.name}} <button class="ml-file-tree__cta--delete" *ngIf="isRemovable(file)" (click)="removeFile.next(file)"><md-icon>delete_forever</md-icon></button>
-      </li>
-    </ul>
-    <div class="ml-file-tree__cta-bar">
-      <button md-button (click)="addFile.next()" class="ml-file-tree__cta">
-        <md-icon>note_add</md-icon> Add file
-      </button>
-    </div>
-  `,
+  templateUrl: './file-tree.component.html',
   styleUrls: ['./file-tree.component.scss']
 })
 export class FileTreeComponent {
@@ -32,11 +16,19 @@ export class FileTreeComponent {
 
   @Output() selectFile = new EventEmitter<File>();
 
+  @Output() editFile = new EventEmitter<File>();
+
   @Output() removeFile = new EventEmitter<File>();
 
   @Output() addFile = new EventEmitter();
 
   isRemovable(file: File) {
     return file.name === MAIN_PYTHON_FILENAME ? false : this.files.length > 1;
+  }
+
+  emitSelectFile(event, file: File) {
+    if (event.target.nodeName === 'LI') {
+      this.selectFile.emit(file);
+    }
   }
 }
