e2f50fcac52ab2fd9b9b990d3316c0c5672e77f2
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-04-05 12:01:08 +0200

    fix: remove socket.io from index

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
-import * as io_lib from 'socket.io';
-
 import { DummyRunner } from './code-runner/dummy-runner.js';
 import { DockerRunner } from './code-runner/docker-runner.js';
 import { AuthService } from './ml-firebase';
 import { MessagingService } from './messaging.service';
 import { createHash } from 'crypto';
 
-const io = io_lib();
 const DUMMY_RUNNER = process.argv.includes('--dummy-runner');
 let runner = DUMMY_RUNNER ? new DummyRunner() : new DockerRunner();
 
