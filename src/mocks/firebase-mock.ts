74d2590082efed2aa665b0c8deaeb2fcb73ecf7b
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-04-10 14:56:02 +0200

    fix: corrects broken test
    This introduces a new testing abstraction
    for firebase and corrects two incorrect tests
    
    Fixes #76

---
+++ src/mocks/firebase-mock.ts
@@ -0,0 +1,41 @@
+export class FirebaseMock {
+  log = [];
+  data = {};
+
+  mockDb() {
+    return new FireBaseDbMock(this);
+  }
+}
+
+
+export class FireBaseDbMock {
+
+  constructor(private fbMock: FirebaseMock) {}
+
+  ref(ref: string) { 
+    return new FirebaseDbRefMock(this.fbMock, ref);
+  }
+}
+
+export class FirebaseDbRefMock {
+
+  constructor(private fbMock: FirebaseMock, private ref: string) {}
+
+  set(data: any) {
+    this.fbMock.data[this.ref] = data;
+    return Promise.resolve(new FirebaseDataSnapshotMock(this.fbMock, data))
+  }
+
+  once(eventType: string) {
+    return Promise.resolve(new FirebaseDataSnapshotMock(this.fbMock, this.fbMock.data[this.ref]))
+  }
+}
+
+export class FirebaseDataSnapshotMock {
+
+  constructor(private fbMock: FirebaseMock, private data: any) {}
+
+  val() {
+    return this.data;
+  }
+}
