15d8477a68eb3706bf203ad02bafa4314f35a839
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-04-12 17:42:35 +0200

    feat: add language helper methods

---
+++ src/app/util/lang.ts
@@ -0,0 +1,12 @@
+export class Lang {
+  static isNullOrUndefined (val) {
+    return val === undefined || val === null;
+  }
+
+  /**
+   * Checks if two params are different. Considers null and undefined equal
+   */
+  static isDifferent(a, b) {
+    return a !== b && !(Lang.isNullOrUndefined(a) && Lang.isNullOrUndefined(b));
+  }
+}
