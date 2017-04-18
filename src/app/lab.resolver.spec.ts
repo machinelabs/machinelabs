machinelabs/machinelabs

commit e29025e0fe56c0d328b149b01adc760401f9c4b4

feat(LabStorage): return Observable<Lab> in createLab

src/app/lab.resolver.ts
@@ -15,8 +15,8 @@ export class LabResolver implements Resolve<Lab> {
 
     // if we don't have any param, create a default lab
     // if we have an id, fetch the lab. If it fails, go for the default lab
-    return !route.params['labid'] 
-            ? Observable.of(this.labStorageService.createLab())
+    return !route.params['labid']
+            ? this.labStorageService.createLab()
             : this.labStorageService.getLab(route.params['labid'])
                                     .map(lab => lab ? lab : this.labStorageService.createLab());
 

src/app/editor-view/editor-view.component.ts
@@ -60,8 +60,10 @@ export class EditorViewComponent implements OnInit {
   }
 
   fork(lab: Lab) {
-    this.lab = this.labStorageService.createLab(lab);
-    this.save(this.lab);
+    this.labStorageService.createLab(lab).subscribe(lab => {
+      this.lab = lab;
+      this.save(this.lab);
+    });
   }
 
   save(lab: Lab) {

src/app/lab-storage.service.ts
@@ -14,16 +14,19 @@ export class LabStorageService {
   constructor(@Inject(DATABASE) private db, private authService: AuthService) {
   }
 
-  createLab(lab?: Lab): Lab {
-    return {
-      id: shortid.generate(),
-      // TODO: we may wanna change the return type to Observable<Lab> and prefill with userId
-      userId: '',
-      name: 'Untitled',
-      description: '',
-      tags: [],
-      files: lab ? lab.files : [{ name: 'main.py', content: DEFAULT_LAB_CODE }]
-    };
+  createLab(lab?: Lab): Observable<Lab> {
+    return this.authService
+      .authenticate()
+      .map(user => {
+        return {
+          id: shortid.generate(),
+          userId: user.uid,
+          name: 'Untitled',
+          description: '',
+          tags: [],
+          files: lab ? lab.files : [{ name: 'main.py', content: DEFAULT_LAB_CODE }]
+        };
+      });
   }
 
   getLab(id: string): Observable<Lab> {

src/app/models/user.ts
@@ -1,4 +1,5 @@
 export interface User {
+  uid: string;
   displayName: string|null;
   email: string|null;
   isAnonymous: boolean;

commit d50378f01a7e3633c3b779e7dedd08323017bb88

fix(LabResolver): fixes bug that resolver emits wrong object when lab doesn't exist
This commit fixes a regression introduced in https://github.com/machinelabs/machinelabs-client/commit/e29025e0fe56c0d328b149b01adc760401f9c4b4, where `LabResolver.resolve()`
resolves with a wrong object type when asking for a lab that doesn't exist (anymore).

src/app/lab.resolver.ts
@@ -18,7 +18,7 @@ export class LabResolver implements Resolve<Lab> {
       // a new empty lab if no lab with the given id exists.
       return this.labStorageService
                   .getLab(route.paramMap.get('labid'))
-                  .map(lab => lab ? lab : this.labStorageService.createLab());
+                  .switchMap(lab => lab ? Observable.of(lab) : this.labStorageService.createLab());
     }
 
     // If a template id is specified, create a lab from that template,

src/app/lab.resolver.spec.ts
@@ -123,5 +123,28 @@ describe('LabResolver', () => {
         expect(lab).toEqual(existingLab);
       });
     });
+
+    it('should resolve with empty lab if labid is given but resolves to null (lab doesn\'t exist', () => {
+
+      let emptyLab: Lab = {
+        id: 'new-lab',
+        user_id: 'user-id',
+        name: 'New Lab',
+        description: 'this is a new lab',
+        tags: [],
+        files: []
+      };
+
+      let activatedRouteSnapshotStub = new ActivatedRouteSnapshot();
+      activatedRouteSnapshotStub.params = { labid: 'some-id' };
+
+      spyOn(labStorageService, 'getLab').and.returnValue(Observable.of(null));
+      spyOn(labStorageService, 'createLab').and.returnValue(Observable.of(emptyLab));
+
+      labResolver.resolve(activatedRouteSnapshotStub).subscribe(lab => {
+        expect(labStorageService.createLab).toHaveBeenCalled();
+        expect(lab).toEqual(emptyLab);
+      });
+    });
   });
 });