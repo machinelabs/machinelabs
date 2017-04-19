e7e5cfb665ce8dc619610785a99299e04049c42a
Pascal Precht <pascal.precht@googlemail.com>
2017-04-19 14:17:08 +0200

    chore(ui/ux): minor markup changes

--- src/app/navigation-confirm-dialog/navigation-confirm-dialog.component.ts
+++ src/app/navigation-confirm-dialog/navigation-confirm-dialog.component.ts
@@ -5,11 +5,16 @@ import { MdDialogRef } from '@angular/material';
   selector: 'ml-navigation-confirm-dialog',
   template: `
     <p>Are you sure? Unsaved changes will be gone.</p>
-    <div style="margin-top: 1em; text-align: center;">
-      <button md-raised-button (click)="dialogRef.close(true)">Yes</button>
-      <button md-raised-button type="button" (click)="dialogRef.close()">Close</button>
+    <div class="cta-bar">
+      <button md-button (click)="dialogRef.close(true)">Yes</button>
+      <button md-button type="button" (click)="dialogRef.close()">Close</button>
     </div>
-  `
+  `,
+  styles: [`
+    .cta-bar {
+      text-align: center;
+    }
+  `]
 })
 export class NavigationConfirmDialogComponent {
 
