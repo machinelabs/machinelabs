0a4eb606a01e21596e5c2838ad39f7113ca2042e
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-04-26 14:31:26 +0200

    feat(ObservableDbRef): add "on" and "value"

--- src/app/firebase/observable-db-ref.ts
+++ src/app/firebase/observable-db-ref.ts
@@ -22,8 +22,16 @@ export class ObservableDbRef {
   }
 
   childAdded(): Observable<firebase.database.DataSnapshot> {
-    return Observable.fromEventPattern(handler => this.ref.on('child_added', handler),
-                                       handler => this.ref.off('child_added', handler));
+    return this.on('child_added');
+  }
+
+  value(): Observable<firebase.database.DataSnapshot> {
+    return this.on('value');
+  }
+
+  on(eventName: string): Observable<firebase.database.DataSnapshot> {
+    return Observable.fromEventPattern(handler => this.ref.on(eventName, handler),
+                                       handler => this.ref.off(eventName, handler));
   }
 }
 
