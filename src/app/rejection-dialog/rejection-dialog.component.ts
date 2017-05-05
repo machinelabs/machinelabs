b77bf8f27876b5d51fd7bc5415e78f2547755e52
Pascal Precht <pascal.precht@googlemail.com>
2017-05-05 18:22:13 +0200

    feat: show rejection dialog when anonymous users try to run lab
    As discussed in #110, anonymous users shouldn't be able to run labs that aren't
    cached. This is already implemented, but so far the only thing we've shown was
    a little snackbar saying that the execution has been rejected.
    
    This commit introduces a new dialog component that encourages users to
    request a beta invite or bake our project on Kickstarter.
    
    Closes #110

---
+++ src/app/rejection-dialog/rejection-dialog.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ml-rejection-dialog',
+  templateUrl: './rejection-dialog.component.html',
+  styleUrls: ['./rejection-dialog.component.scss']
+})
+export class RejectionDialogComponent {}
