cde18c1da1ae273170677277d34a25bf27993a34
Pascal Precht <pascal.precht@googlemail.com>
2017-05-05 16:21:29 +0200

    feat: introduce `SharedModule`
    This is needed to use e.g. UI component across modules that are lazy loaded.
    Also introduces a new component `TagListComponent`.

---
+++ src/app/tag-list/tag-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'ml-tag-list',
+  template: `
+    <md-chip-list>
+      <md-chip *ngFor="let tag of tags">{{tag}}</md-chip>
+    </md-chip-list>
+  `,
+  styleUrls: ['./tag-list.component.scss']
+})
+export class TagListComponent {
+
+  @Input() tags = [];
+}
