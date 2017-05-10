948c5330ebff3a84e21bea9ff0f91903199503ee
Carmen Popoviciu <carmen@widespread.com>
2017-05-10 09:41:40 +0300

    feat(e2e): add basic suite of e2e tests

---
+++ e2e/editor-view/editor-view.e2e-spec.ts
@@ -0,0 +1,20 @@
+import { browser } from 'protractor';
+import { EditorViewPageObject } from './editor-view.pageObject';
+
+describe('Editor-View Component', function() {
+  let editorView: EditorViewPageObject;
+
+  beforeEach(() => {
+    editorView = new EditorViewPageObject();
+    browser.get('/editor?file=main.py');
+    browser.sleep(3000);
+  });
+
+  it('should contain the "Editor" and "Console" tabs', () => {
+    expect(editorView.tabsLabels).toEqual(['Editor', 'Console']);
+  });
+
+  it('should have the "Editor" tab selected by default', () => {
+    expect(editorView.activeTabLabel).toEqual('Editor');
+  });
+});
