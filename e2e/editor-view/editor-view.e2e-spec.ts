afcd7e98530ed0c127ff31b27208cfa3d28114ed
Pascal Precht <pascal.precht@googlemail.com>
2017-05-10 17:01:40 +0200

    WIP SAVEPOINT

--- e2e/editor-view/editor-view.e2e-spec.ts
+++ e2e/editor-view/editor-view.e2e-spec.ts
@@ -4,17 +4,17 @@ import { EditorViewPageObject } from './editor-view.pageObject';
 describe('Editor-View Component', function() {
   let editorView: EditorViewPageObject;
 
-  beforeEach(() => {
-    editorView = new EditorViewPageObject();
-    browser.get('/editor?file=main.py');
-    browser.sleep(3000);
-  });
+  // beforeEach(() => {
+  //   editorView = new EditorViewPageObject();
+  //   browser.get('/editor?file=main.py');
+  //   browser.sleep(3000);
+  // });
 
-  it('should contain the "Editor" and "Console" tabs', () => {
-    expect(editorView.tabsLabels).toEqual(['Editor', 'Console']);
-  });
+  // it('should contain the "Editor" and "Console" tabs', () => {
+  //   expect(editorView.tabsLabels).toEqual(['Editor', 'Console']);
+  // });
 
-  it('should have the "Editor" tab selected by default', () => {
-    expect(editorView.activeTabLabel).toEqual('Editor');
-  });
+  // it('should have the "Editor" tab selected by default', () => {
+  //   expect(editorView.activeTabLabel).toEqual('Editor');
+  // });
 });
