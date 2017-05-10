383ede8345a4cd54eaa9db4187e120d845d2e7ae
Pascal Precht <pascal.precht@googlemail.com>
2017-05-10 13:11:04 +0200

    feat: make ml-dialog themable

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ml-dialog-header',
-  template: `<h4><ng-content></ng-content></h4>`,
+  template: `<h4 class="ml-dialog-headline"><ng-content></ng-content></h4>`,
   styleUrls: ['./dialog-header.component.scss']
 })
 export class DialogHeaderComponent {}
 
 @Component({
   selector: 'ml-dialog-content',
-  template: `<ng-content></ng-content>`,
+  template: `<div class="ml-dialog-content"><ng-content></ng-content></div>`,
   styleUrls: ['./dialog-content.component.scss']
 })
 export class DialogContentComponent {}
