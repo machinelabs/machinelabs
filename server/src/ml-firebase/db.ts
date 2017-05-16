machinelabs/machinelabs

commit f6e4012fd23e36dbfc6f58d303eca079a9786cd4

feat: use firebase-admin for authentication
Previously we used an email/password combination
to authenticate the server against the firebase.

Since such a user gets a random, non-changeable
uid from firebase it was impossible to create
firebase rules that could be shared across
different environments (production, staging, dev[s]).

With firebase-admin we authenticate using
a private key and private email and can set
the uid from the server. The server now uses
a uid named `execution-server`. This makes
it possible to write reusable firebase
security rules.

In order for this to work against your local
dev setup make sure to:

1. Create credentials for firebase-admin from the website
2. Store them in the following env vars

MACHINELABS_FB_PRIVATE_KEY
MACHINELABS_FB_CLIENT_EMAIL

3. Make sure to replace all new line characters
in MACHINELABS_FB_PRIVATE_KEY with actual new lines

Closes #45

server/src/ml-firebase/db.ts
@@ -1,4 +1,15 @@
-import * as firebase from 'firebase';
-import { environment } from '../environments/environment';
+import * as admin from 'firebase-admin';
+import { Config } from '../util/config';
 
-export const db = this.db = firebase.initializeApp(environment.firebaseConfig).database();
+admin.initializeApp({
+  credential: admin.credential.cert(<any>{
+    "private_key": Config.getEnv(Config.ENV_PRIVATE_KEY),
+    "client_email": Config.getEnv(Config.ENV_CLIENT_EMAIL),
+  }),
+  databaseURL: 'https://machinelabs-dev.firebaseio.com',
+  databaseAuthVariableOverride: {
+    uid: 'execution-server'
+  }
+});
+
+export let db = admin.database();

server/src/index.ts
@@ -1,6 +1,5 @@
 import { DummyRunner } from './code-runner/dummy-runner.js';
 import { DockerRunner } from './code-runner/docker-runner.js';
-import { AuthService } from './ml-firebase';
 import { MessagingService } from './messaging.service';
 import { RulesService } from './rules.service';
 
@@ -8,8 +7,7 @@ const DUMMY_RUNNER = process.argv.includes('--dummy-runner');
 let runner = DUMMY_RUNNER ? new DummyRunner() : new DockerRunner();
 
 const rulesService = new RulesService();
-const authService = new AuthService();
-const messagingService = new MessagingService(authService, rulesService, runner);
+const messagingService = new MessagingService(rulesService, runner);
 messagingService.init();
 
 console.log(`Using runner: ${runner.constructor.name}`);

server/src/messaging.service.ts
@@ -1,6 +1,6 @@
 import * as firebase from 'firebase';
 import { Crypto } from './util/crypto';
-import { db, AuthService, DbRefBuilder } from './ml-firebase';
+import { db, DbRefBuilder } from './ml-firebase';
 import { CodeRunner, ProcessStreamData } from './code-runner/code-runner';
 import { Observable } from '@reactivex/rxjs';
 import { Invocation, InvocationType } from './models/invocation';
@@ -14,29 +14,25 @@ export class MessagingService {
   // TODO: Move the infos somewhere else as soon as we start providing different stacks
   SERVER_INFO = '1 vCPU Intel Ivy Bridge';
 
-  constructor(private authService: AuthService,
-              private rulesService: RulesService,
+  constructor(private rulesService: RulesService,
               private codeRunner: CodeRunner) {
   }
 
   init() {
     // Listen on all incoming runs to do the right thing
-    this.authService
-        .authenticate()
-        .switchMap(_ => this.db.newInvocationsRef().childAdded())
-        .map(snapshot => snapshot.val())
-        .switchMap(invocation => this.getOutputAsObservable(invocation))
-        .switchMap(data => this.writeExecutionMessage(data.output, data.invocation))
-        .subscribe();
+  this.db.newInvocationsRef().childAdded()
+         .map(snapshot => snapshot.val())
+         .switchMap(invocation => this.getOutputAsObservable(invocation))
+         .switchMap(data => this.writeExecutionMessage(data.output, data.invocation))
+         .subscribe();
 
     // Listen on all changed runs to get notified about stops
-    this.authService
-        .authenticate()
-        .switchMap(_ =>this.db.invocationsRef().childChanged())
-        .map(snapshot => snapshot.val())
-        .filter(execution => execution.type === InvocationType.StopExecution)
-        .subscribe(execution => this.codeRunner.stop(execution));
+    this.db.invocationsRef().childChanged()
+           .map(snapshot => snapshot.val())
+           .filter(execution => execution.type === InvocationType.StopExecution)
+           .subscribe(execution => this.codeRunner.stop(execution));
   }
+
 
   /**
    * Take a run and observe the output. The run maybe cached or rejected

server/src/ml-firebase/auth.service.ts
@@ -1,24 +1,0 @@
-import * as firebase from 'firebase';
-import { Observable } from '@reactivex/rxjs';
-import { Config } from '../util/config';
-
-export class AuthService {
-
-  private login$: Observable<any>;
-
-  authenticate(): Observable<any> {
-    if (!this.login$) {
-      this.login$ = Observable.fromPromise(new Promise(resolve => firebase.auth().onAuthStateChanged(resolve))
-                                .then(user => user ? user : firebase.auth()
-                                                                    .signInWithEmailAndPassword(
-                                                                      Config.getEnv(Config.ENV_USERNAME),
-                                                                      Config.getEnv(Config.ENV_PASSWORD)
-                                                                    ))
-                              )
-                              .publishLast()
-                              .refCount();
-    }
-
-    return this.login$;
-  }
-}

server/src/ml-firebase/index.ts
@@ -1,4 +1,3 @@
-export * from './auth.service';
 export * from './db';
 export * from './db-ref-builder';
 export * from './observable-db-ref';

server/src/util/config.ts
@@ -4,8 +4,8 @@
  */
 
 export class Config {
-  static readonly ENV_USERNAME = 'MACHINELABS_FB_BOT_USERNAME';
-  static readonly ENV_PASSWORD = 'MACHINELABS_FB_BOT_PASSWORD';
+  static readonly ENV_PRIVATE_KEY = 'MACHINELABS_FB_PRIVATE_KEY';
+  static readonly ENV_CLIENT_EMAIL = 'MACHINELABS_FB_CLIENT_EMAIL';
 
   static getEnv (name: string) {
     if (!process.env[name]){

commit 95462ec693884187c27512c16687cf0809738a88

fix: use databaseURL specified in environment file
https://github.com/machinelabs/machinelabs-server/commit/f6e4012fd23e36dbfc6f58d303eca079a9786cd4 introduced a bug that always the same firebase `databaseURL` has been
used instead of the one speficied in a dedicated environment file.

This caused the server to try to connect to the wrong database if the one specified
in environment had a different endpoint.

server/src/ml-firebase/db.ts
@@ -1,4 +1,5 @@
 import * as admin from 'firebase-admin';
+import { environment } from '../environments/environment';
 import { Config } from '../util/config';
 
 admin.initializeApp({
@@ -6,7 +7,7 @@ admin.initializeApp({
     "private_key": Config.getEnv(Config.ENV_PRIVATE_KEY),
     "client_email": Config.getEnv(Config.ENV_CLIENT_EMAIL),
   }),
-  databaseURL: 'https://machinelabs-dev.firebaseio.com',
+  databaseURL: environment.firebaseConfig.databaseURL,
   databaseAuthVariableOverride: {
     uid: 'execution-server'
   }