397ce9af2942986570d4f972dec9dce99eeb92e4
Pascal Precht <pascal.precht@googlemail.com>
2017-05-30 14:24:21 +0200

    feat(UserProfile): let users edit their profile info
    This can land once https://github.com/machinelabs/machinelabs-server/pull/68 has been merged.
    
    Closes #153

--- src/app/user-profile/user-profile.module.ts
+++ src/app/user-profile/user-profile.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { SharedModule } from '../shared/shared.module';
 import { MachineLabsMaterialModule } from '../ml-material.module';
@@ -11,9 +12,12 @@ import { UserResolver, UserLabsResolver } from './user.resolver';
 
 import { USER_PROFILE_ROUTES } from './user-profile.routes';
 
+import { EditUserProfileDialogComponent } from './edit-user-profile-dialog/edit-user-profile-dialog.component';
+
 @NgModule({
   declarations: [
-    UserProfileComponent
+    UserProfileComponent,
+    EditUserProfileDialogComponent
   ],
   providers: [
     UserResolver,
@@ -22,10 +26,14 @@ import { USER_PROFILE_ROUTES } from './user-profile.routes';
   imports: [
     CommonModule,
     SharedModule,
+    ReactiveFormsModule,
     MachineLabsMaterialModule,
     FlexLayoutModule,
     RouterModule.forChild(USER_PROFILE_ROUTES),
     ToolbarModule
+  ],
+  entryComponents: [
+    EditUserProfileDialogComponent
   ]
 })
 export class UserProfileModule {}
