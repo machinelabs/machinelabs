e54a77b6df25543046f943b0cfcc2b8fe926e4a9
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-06-04 21:36:56 +0200

    feat(LabConfigService): add service to read ml.yaml

---
+++ server/src/lab-config/lab-config.service.ts
@@ -0,0 +1,35 @@
+import { Lab } from '../models/lab';
+import { PublicLabConfiguration } from '../models/lab-configuration';
+import { safeLoad} from 'js-yaml';
+
+const CONFIG_FILE_NAME = 'ml.yaml';
+
+export class LabConfigService {
+  readConfig(lab: Lab) {
+
+    let configFile = this.getMlYaml(lab);
+
+    if (!configFile || !configFile.content){
+      return null;
+    }
+
+    try {
+      let config = safeLoad(configFile.content);
+
+      let actualConfig = Object.assign(new PublicLabConfiguration(), config);
+      return this.isValidConfig(actualConfig) ? actualConfig : null;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  private getMlYaml(lab: Lab) {
+    let file = lab.directory
+                  .find(file => file.name.toLowerCase() === CONFIG_FILE_NAME);
+    return file;
+  }
+
+  private isValidConfig(config: PublicLabConfiguration) {
+    return !!config.dockerImageId;
+  }
+}
