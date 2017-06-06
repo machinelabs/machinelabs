a3f771c96c629fe0ff2fb65526e8dbc26ceef8c5
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-06-06 12:01:50 +0200

    feat(ml.yaml): provide default file

---
+++ src/app/data/ml.yaml.ts
@@ -0,0 +1,17 @@
+export const ML_YAML = `
+
+# Set the dockerImageId to choose between different environments
+# 
+# Currently supported:
+#
+# dockerImageId                  |      description
+# keras_v2-0-x_python_3-1        | Keras 2.0.4 and Python 3.1
+# keras_v2-0-x_python_2-1        | Keras 2.0.4 and Python 2.1
+
+dockerImageId: keras_v2-0-x_python_3-1
+`;
+
+export const ML_YAML_FILE = {
+  name: 'ml.yaml',
+  content: ML_YAML
+};
