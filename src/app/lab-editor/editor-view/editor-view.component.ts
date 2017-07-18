machinelabs/machinelabs

commit 48e2d31ce8fad8f738d7e6552544460a835ecb07

fix: ensure execution id updates in url when running/restarting labs
This didn't work before because executing or restarting a lab did not update the
URL accordingly, resulting in the UI showing a wrong state.

With this commits we also refactor `RleService.run()` as we need synchronous access
to the newly created execution id.

src/app/lab-editor/editor-view/editor-view.component.ts
@@ -120,7 +120,7 @@ export class EditorViewComponent implements OnInit {
 
     this.route.paramMap
       .map(paramMap => paramMap.get('executionId'))
-      .filter(executionId => !!executionId && executionId != this.activeExecutionId)
+      .filter(executionId => !!executionId && executionId !== this.activeExecutionId)
       .subscribe(executionId => {
         this.activeExecutionId = executionId;
         this.consume(this.rleService.listen(executionId));
@@ -149,28 +149,29 @@ export class EditorViewComponent implements OnInit {
   }
 
   run(lab: Lab) {
-
     // First check if this lab is already persisted or not. We don't want to
     // execute labs that don't exist in the database.
     this.labStorageService.labExists(lab.id)
-        .switchMap(exists => exists ? Observable.of(null) :
-            // If it doesn't exist yet, we save it first and make sure to update
-            // the url accordingly, so it can be easily shared. We can't use
-            // router.navigate() here because it will perform a full navigation
-            // cycle as gonig from /editor to /editor/:id is a route change.
-            this.labStorageService.saveLab(lab)
-                .do(_ => this.goToLab(lab))
-        )
-        .subscribe(() => {
-          let wrapper = this.rleService.run(lab);
-          this.consume(wrapper);
+        // If it doesn't exist yet, we save it first and make sure to update
+        // the url accordingly, so it can be easily shared.
+        .switchMap(exists => exists ? Observable.of(null) : this.labStorageService.saveLab(lab))
+        .subscribe(_ => {
+          const executionId = this.rleService.run(lab);
+
+          // A new execution also means a new execution id. We update the
+          // query parameter accordingly so the correct state is represented
+          // in the UI.
+          this.router.navigate(['/editor', lab.id, executionId], {
+            queryParamsHandling: 'merge',
+            relativeTo: this.route
+          });
         });
   }
 
-  consume(wrapper: Observable<ExecutionWrapper>) {
+  consume(wrapper: ExecutionWrapper) {
     this.outputPanel.clear();
     this.selectTab(TabIndex.Console);
-    this.execution = wrapper.switchMap(val => val.execution);
+    this.execution = wrapper.execution;
 
     if (this.executionSubscription) {
       this.executionSubscription.unsubscribe();
@@ -184,7 +185,7 @@ export class EditorViewComponent implements OnInit {
                                      .take(1)
                                      .subscribe(execution => this.initDirectory(execution.lab.directory));
 
-    let messages$ = wrapper.switchMap(val => val.messages);
+    let messages$ = wrapper.messages;
 
     // Scan the notifications and build up a string with line breaks
     // Don't make this a manual subscription without dealing with

src/app/lab-editor/has-execution.guard.ts
@@ -36,11 +36,11 @@ export class HasExecutionGuard implements CanActivate {
     // navigating to a lab that simply hasn't been executed yet.
     return this.labExecutionService
         .getLatestExecutionIdForLab(labId)
-        .map(executionId => {
-          if (!executionId) {
+        .map(_executionId => {
+          if (!_executionId) {
             return true
           }
-          this.router.navigate(['/editor', labId, executionId], {
+          this.router.navigate(['/editor', labId, _executionId], {
             queryParamsHandling: 'merge',
             relativeTo: this.route
           });

src/app/lab-editor/remote-code-execution/remote-lab-exec.service.spec.ts
@@ -149,27 +149,26 @@ describe('RemoteLabExecService', () => {
         expect(executionsSubscriber).toBe(1);
       });
 
-      rleService.consumeExecution(messages$, executions$)
-                .subscribe((ew) => {
-                  ew.messages
-                    .do(msg => actualMessages.push(msg))
-                    .finally(() => {
-                      expect(actualMessages.length).toBe(3);
-                      doneWhen.call();
-                    })
-                    .subscribe();
+      const wrapper = rleService.consumeExecution('someid', messages$, executions$);
 
-                  ew.execution
-                    .do(e => {
-                      console.log(e);
-                      actualExecutions.push(e);
-                    })
-                    .finally(() => {
-                      expect(actualExecutions.length).toBe(2);
-                      doneWhen.call();
-                    })
-                    .subscribe();
-                });
+      wrapper.messages
+              .do(msg => actualMessages.push(msg))
+              .finally(() => {
+                expect(actualMessages.length).toBe(3);
+                doneWhen.call();
+              })
+              .subscribe();
+
+      wrapper.execution
+              .do(e => {
+                console.log(e);
+                actualExecutions.push(e);
+              })
+              .finally(() => {
+                expect(actualExecutions.length).toBe(2);
+                doneWhen.call();
+              })
+              .subscribe();
     });
 
     it('should handle ExecutionRejected gracefully', (done) => {
@@ -221,28 +220,27 @@ describe('RemoteLabExecService', () => {
         expect(executionsSubscriber).toBe(1);
       });
 
-      rleService.consumeExecution(messages$, executions$)
-                .subscribe((ew) => {
-                  ew.messages
-                    .do(msg => actualMessages.push(msg))
-                    .finally(() => {
-                      expect(actualMessages.length).toBe(1);
-                      console.log('messages$ completed');
-                      doneWhen.call();
-                    })
-                    .subscribe();
+      const wrapper = rleService.consumeExecution('someid', messages$, executions$);
 
-                  ew.execution
-                    .do(e => {
-                      actualExecutions.push(e);
-                    })
-                    .finally(() => {
-                      expect(actualExecutions.length).toBe(0);
-                      console.log('execution$ completed');
-                      doneWhen.call();
-                    })
-                    .subscribe();
-                });
+      wrapper.messages
+              .do(msg => actualMessages.push(msg))
+              .finally(() => {
+                expect(actualMessages.length).toBe(1);
+                console.log('messages$ completed');
+                doneWhen.call();
+              })
+              .subscribe();
+
+      wrapper.execution
+              .do(e => {
+                actualExecutions.push(e);
+              })
+              .finally(() => {
+                expect(actualExecutions.length).toBe(0);
+                console.log('execution$ completed');
+                doneWhen.call();
+              })
+              .subscribe();
     });
   });
 });

src/app/lab-editor/remote-code-execution/remote-lab-exec.service.ts
@@ -3,7 +3,13 @@ import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 import { Lab } from '../../models/lab';
 import { Invocation, InvocationType } from '../../models/invocation';
-import { Execution, ExecutionStatus, ExecutionMessage, MessageKind, ExecutionWrapper } from '../../models/execution';
+import {
+  Execution,
+  ExecutionStatus,
+  ExecutionMessage,
+  MessageKind,
+  ExecutionWrapper
+} from '../../models/execution';
 import { MessageStreamOptimizer } from './message-stream-optimizer';
 
 import * as shortid from 'shortid';
@@ -32,7 +38,7 @@ export class RemoteLabExecService {
     this.messageStreamOptimizer = new MessageStreamOptimizer(db, this.PARTITION_SIZE, this.FULL_FETCH_TRESHOLD);
   }
 
-  run(lab: Lab): Observable<ExecutionWrapper> {
+  run(lab: Lab): string {
     let id = this.newInvocationId();
     let executionWrapper$ = this.authService
       .requireAuthOnce()
@@ -46,12 +52,12 @@ export class RemoteLabExecService {
           directory: lab.directory
         }
       }))
-      .switchMap(_ => this.listen(id));
+      .subscribe();
 
-    return executionWrapper$;
+    return id;
   }
 
-  listen(executionId: string): Observable<ExecutionWrapper> {
+  listen(executionId: string): ExecutionWrapper {
 
     let execution$ = this.db.executionRef(executionId)
                             .value()
@@ -59,10 +65,10 @@ export class RemoteLabExecService {
 
     let messages$ = this.messageStreamOptimizer.listenForMessages(executionId);
 
-    return this.consumeExecution(messages$, execution$);
+    return this.consumeExecution(executionId, messages$, execution$);
   }
 
-  consumeExecution(messages: Observable<ExecutionMessage>, execution: Observable<Execution>): Observable<ExecutionWrapper> {
+  consumeExecution(executionId: string, messages: Observable<ExecutionMessage>, execution: Observable<Execution>): ExecutionWrapper {
 
     let sharedMessages = messages.share();
     let sharedExecution = execution.share();
@@ -82,10 +88,11 @@ export class RemoteLabExecService {
                       .takeWhileInclusive(executingExecutions)
                       .takeUntil(rejectedMessage);
 
-    return Observable.of({
+    return {
+      executionId: executionId,
       messages: messages$,
       execution: execution$
-    });
+    };
   }
 
   stop(executionId: string) {

src/app/models/execution.ts
@@ -62,6 +62,8 @@ export class ExecutionRejectionInfo {
 }
 
 export interface ExecutionWrapper {
-  messages: Observable<ExecutionMessage>;
+  executionId: string;
   execution: Observable<Execution>;
+  messages: Observable<ExecutionMessage>;
 }
+

commit 20b33da1ff2e4d0c33f2a7502e753ac7937afa00

fix: don't listen to latest execution in list when initializing lab
A regression was introduced in https://github.com/machinelabs/machinelabs-client/commit/48e2d31ce8fad8f738d7e6552544460a835ecb07
that `rleService.listen()` was executed twice in lab initialization.
First with the execution id provided via URL (the one determined by
`HasValidExecutionGuard`), and then a second time in `initLab()` where
we, up until now, simply picked the first execution of the list and
listened to it as well, which essentially caused the output to be
overwritten while the correct execution is selected.

src/app/lab-editor/editor-view/editor-view.component.ts
@@ -358,12 +358,10 @@ export class EditorViewComponent implements OnInit {
         .take(1)
         .map(executions => executions.length > 0 ? executions[0] : null)
         .filter(obsExecution => !!obsExecution)
-        .switchMap(obsExecution => obsExecution)
         .subscribe(execution => {
           // Only attach to an existing execution if the user did not do it by themself
           if (this.clientExecutionState === ClientExecutionState.NotExecuting) {
             this.selectTab(TabIndex.Console);
-            this.listen(execution);
           }
         });
 