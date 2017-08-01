8bb6447e5deaea156658ead8cbae9430a8dfd4bb
Pascal Precht <pascal.precht@googlemail.com>
2017-08-01 22:24:48 +0200

    feat: introduce embed dialog
    Closes #328

---
+++ client/src/app/lab-editor/embed-dialog/embed-dialog.component.ts
@@ -0,0 +1,41 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MD_DIALOG_DATA } from '@angular/material';
+import { LocationHelper } from '../../util/location-helper';
+
+@Component({
+  selector: 'ml-embed-dialog',
+  templateUrl: './embed-dialog.component.html',
+  styles: [`
+    textarea {
+      width: 100%;
+      height: 175px;
+      font-size: 0.9em;
+      border: 1px solid #ccc;
+    }
+    p {
+      text-align: center;
+    }
+  `]
+})
+export class EmbedDialogComponent implements OnInit {
+
+  embedUrl: string;
+
+  editorUrl: string;
+
+  constructor(
+    @Inject(MD_DIALOG_DATA) public data,
+    private locationHelper: LocationHelper
+  ) {}
+
+  ngOnInit() {
+    this.embedUrl = this.locationHelper.prepareExternalUrl(
+      ['/embedded', this.data.lab.id, this.data.executionId],
+      true
+    );
+    this.editorUrl = this.locationHelper.prepareExternalUrl(
+      ['/editor', this.data.lab.id, this.data.executionId],
+      true
+    );
+  }
+}
