d212b1b79a0d4ef7298ca51dae47efcdf7d75441
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-08-01 07:43:13 +0200

    chore: add env files for production

---
+++ client/src/environments/environment.production.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  offline: false,
+  testing: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDB6mmk8CDyy9D6DpYQgzLd3-wwY5WDSEc',
+    authDomain: 'machinelabs-production.firebaseapp.com',
+    databaseURL: 'https://machinelabs-production.firebaseio.com',
+    projectId: 'machinelabs-production',
+    storageBucket: 'machinelabs-production.appspot.com',
+    messagingSenderId: '273257478502'
+  }
+};
+
