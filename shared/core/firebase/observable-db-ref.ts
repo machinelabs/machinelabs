5933ad932cb2fee291da3a75ae4157a800c614d2
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-08-09 11:46:08 +0200

    feat(machinelabs-core): create shared lib

---
+++ shared/core/firebase/observable-db-ref.ts
@@ -0,0 +1,65 @@
+import { Observable } from '@reactivex/rxjs';
+import * as firebase from 'firebase';
+
+export class ObservableDbRef {
+
+  ref: any;
+
+  constructor (ref: any) {
+    this.ref = ref;
+  }
+
+  limitToLast(limit: number) {
+    return new ObservableDbRef(this.ref.limitToLast(limit));
+  }
+
+  limitToFirst(limit: number) {
+    return new ObservableDbRef(this.ref.limitToFirst(limit));
+  }
+
+  orderByChild(value: any) {
+    return new ObservableDbRef(this.ref.orderByChild(value));
+  }
+
+  startAt(value: any, key?: any) {
+    return new ObservableDbRef(this.ref.startAt(value, key));
+  }
+
+  endAt(value: any, key?: any) {
+    return new ObservableDbRef(this.ref.endAt(value, key));
+  }
+
+  once(eventType: string): Observable<firebase.database.DataSnapshot> {
+    return Observable.fromPromise(this.ref.once(eventType));
+  }
+
+  onceValue(): Observable<firebase.database.DataSnapshot> {
+    return this.once('value');
+  }
+
+  set(data: any): Observable<firebase.database.DataSnapshot> {
+    return Observable.fromPromise(this.ref.set(data));
+  }
+
+  update(data: any): Observable<firebase.database.DataSnapshot> {
+    return Observable.fromPromise(this.ref.update(data));
+  }
+
+  childAdded(): Observable<firebase.database.DataSnapshot> {
+    return this.on('child_added');
+  }
+
+  childChanged(): Observable<firebase.database.DataSnapshot> {
+    return this.on('child_changed');
+  }
+
+  value(): Observable<firebase.database.DataSnapshot> {
+    return this.on('value');
+  }
+
+  on(eventName: string): Observable<firebase.database.DataSnapshot> {
+    return Observable.fromEventPattern(handler => this.ref.on(eventName, handler),
+                                       handler => this.ref.off(eventName, handler));
+  }
+}
+
