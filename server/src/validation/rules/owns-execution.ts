e830c97ed0bcdd10bc72c16fc172bb1e97a86ee6
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-08-10 22:43:01 +0200

    refactor(server): use models from @machinelabs/core

--- server/src/validation/rules/owns-execution.ts
+++ server/src/validation/rules/owns-execution.ts
@@ -2,7 +2,7 @@ import { Observable } from '@reactivex/rxjs'
 import { ValidationRule } from './rule';
 import { Invocation } from '../../models/invocation';
 import { ValidationResult } from '../validation-result';
-import { Execution } from '../../models/execution';
+import { Execution } from '@machinelabs/core';
 import { ExecutionResolver } from '../resolver/execution-resolver';
 
 export class OwnsExecutionRule implements ValidationRule {
