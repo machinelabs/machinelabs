25db682c7fa4af6bebbd70974bf43bb8bc4008ec
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-08-10 22:37:42 +0200

    chore(@machinelabs/core): add things to barrel

--- shared/core/index.ts
+++ shared/core/index.ts
@@ -1,1 +1,6 @@
+export * from './firebase/db-ref-builder';
 export * from './firebase/observable-db-ref';
+export * from './models/execution';
+export * from './models/server';
+export * from './models/months';
+export * from './models/lab';
