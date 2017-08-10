46a68202fffef98efe31148fd572061ccfa294e0
Pascal Precht <pascal.precht@googlemail.com>
2017-08-10 16:30:58 +0200

    feat: introduce user_visible_labs node and hidden property in Lab type
    This also updates our post-lab-write cloud function to update the
    `user_visible_labs` node + update all associated `user_visible_executions`
    as well

--- firebase/functions/src/post-lab-write.ts
+++ firebase/functions/src/post-lab-write.ts
@@ -7,18 +7,40 @@ import * as crypto from 'crypto';
 
 
 export const postLabWrite = functions.database.ref('/labs/{id}/common')
-  .onWrite(event => Promise.all([saveUserLabId(event), setHasCachedRun(event)]));
+  .onWrite(event => Promise.all([updateIndices(event), setHasCachedRun(event)]));
 
 function hashDirectory(directory) {
   const hasher = crypto.createHash('sha256');
   return hasher.update(JSON.stringify(directory)).digest('hex');
 }
 
-function saveUserLabId(event) {
+function updateIndices(event) {
+  let delta = {};
   const data = event.data.val();
-  return admin.database()
-              .ref(`idx/user_labs/${data.user_id}/${data.id}`)
-              .set(true);
+
+  console.log('Post lab write: updating indices');
+
+  delta[`/idx/user_labs/${data.user_id}/${data.id}`] = true;
+  delta[`/idx/user_visible_labs/${data.user_id}/${data.id}`] = data.hidden ? null : true;
+
+
+  // We need to find all executions that are attached to this lab
+  // and hide them as well.
+  //
+  // Notice we don't update `lab_visible_executions` because we only
+  // need to hide them for the users not for the labs.
+  return admin.database().ref('executions')
+    .orderByChild('common/lab/id')
+    .equalTo(data.id)
+    .once('value')
+    .then(snapshot => snapshot.val())
+    .then(val => val ? Object.keys(val) : [])
+    .then(executionIds => {
+      executionIds.forEach(id => {
+        delta[`/idx/user_visible_executions/${data.user_id}/${id}`] = data.hidden ? null : true;
+      });
+    })
+    .then(_ => admin.database().ref().update(delta));
 }
 
 function setHasCachedRun(event) {
