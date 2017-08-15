7dc0d42234cd751ec2911ca9b3c434eae0de9163
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-08-15 10:36:41 +0200

    feat(client): fine tune rejection messages
    Fixes #391

--- client/src/app/lab-editor/rejection-dialog/rejection-dialog.component.ts
+++ client/src/app/lab-editor/rejection-dialog/rejection-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MD_DIALOG_DATA } from '@angular/material';
 import { ExecutionRejectionReason } from '../../models/execution';
+import { LoginService } from '../../login.service';
 
 @Component({
   selector: 'ml-rejection-dialog',
@@ -9,5 +10,10 @@ import { ExecutionRejectionReason } from '../../models/execution';
 })
 export class RejectionDialogComponent {
   ExecutionRejectionReason = ExecutionRejectionReason
-  constructor(@Inject(MD_DIALOG_DATA) public data: any) {}
+  constructor(@Inject(MD_DIALOG_DATA) public data: any,
+              public loginService: LoginService) {}
+
+  loginWithGithub() {
+    this.loginService.loginWithGitHub();
+  }
 }
