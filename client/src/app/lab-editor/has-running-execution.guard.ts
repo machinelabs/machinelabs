0102a2266345ceb50b21d71b9436144dedcbefb7
Pascal Precht <pascal.precht@googlemail.com>
2017-08-15 20:58:46 +0200

    fix: ensure HasRunningExecutionGuard dialog only shows up for owners
    Fixes #394

--- client/src/app/lab-editor/has-running-execution.guard.ts
+++ client/src/app/lab-editor/has-running-execution.guard.ts
@@ -3,6 +3,8 @@ import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angu
 import { MdDialog } from '@angular/material';
 import { Observable } from 'rxjs/Observable';
 
+import { UserService } from '../user/user.service';
+import { LabStorageService } from '../lab-storage.service';
 import { EditorViewComponent } from './editor-view/editor-view.component';
 import {
   NavigationConfirmDialogComponent,
@@ -11,10 +13,19 @@ import {
 
 import { LabExecutionService } from '../lab-execution.service';
 
+import 'rxjs/add/observable/forkJoin';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/switchMap';
+
 @Injectable()
 export class HasRunningExecutionGuard implements CanDeactivate<EditorViewComponent> {
 
-  constructor (private labExecutionService: LabExecutionService, private dialog: MdDialog) {}
+  constructor (
+    private labExecutionService: LabExecutionService,
+    private dialog: MdDialog,
+    private userService: UserService,
+    private labStorageService: LabStorageService
+  ) {}
 
   canDeactivate(
     component: EditorViewComponent,
@@ -24,13 +35,29 @@ export class HasRunningExecutionGuard implements CanDeactivate<EditorViewCompone
   ) {
     const labId = currentRoute.paramMap.get('id');
 
+    // if we're about to navigate to another execution
+    // e.g. the current labId is in the next state as well,
+    // we can simply resolve because we aren't leaving execution
+    // view.
     if (nextState.url.indexOf(labId) > -1) {
       return Observable.of(true);
     }
 
-    return this.labExecutionService
+    return Observable.forkJoin(
+      this.labStorageService.getLab(labId),
+      this.userService.observeUserChanges().take(1)
+    )
+    // It might not be obvious why we're doing a forkJoin here, but we have to
+    // resolve the loggedin user first so we can access state in isLoggedInUser
+    .switchMap(val => this.userService.isLoggedInUser(val[0].user_id))
+    .switchMap(userOwnsLab => {
+      if (!userOwnsLab) {
+        return Observable.of(true);
+      }
+      return this.labExecutionService
         .labHasRunningExecutions(labId)
         .switchMap(val => val ? this.showDialog() : Observable.of(true));
+    });
   }
 
   private showDialog() {
