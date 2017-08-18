8ceab261052a4e7c0e09f87850f43eed786697be
Pascal Precht <pascal.precht@googlemail.com>
2017-08-18 14:11:48 +0200

    feat: add ga integration and ensure url changes are being tracked
    Autotrack makes does most of the work here as it sends virtual page view
    events to GA when the URL changes.
    
    Closes #397

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,12 +1,21 @@
 import './polyfills.ts';
+import 'autotrack';
+import 'autotrack/lib/plugins/url-change-tracker';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/';
 
+declare const ga: any;
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
+  if (environment.production) {
+    ga('require', 'urlChangeTracker');
+    ga('send', 'pageview');
+  }
+});
