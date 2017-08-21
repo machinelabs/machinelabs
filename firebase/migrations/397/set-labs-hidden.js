24b133f4c5c79c283b5262aa97bbd58cb61454ba
Pascal Precht <pascal.precht@googlemail.com>
2017-08-21 13:39:54 +0200

    chore: add migration script
    This script migrates existing labs to align with the latest schema changes:
    
    - `Lab` now has a `hidden` property (default `false`)
    - New `user_visible_labs` index represents all visible labs for a user
    
    Prior to this commit we don't didn't have a `hidden` property in labs.
    That means the decent default for all labs that need migration is adding
    a `hidden` property to all existing labs and set it to `false`.
    
    At the same time, all labs that will be touched can also be added to the
    new `user_visible_labs` index, as all labs will be visible by default.
    
    To run this script execute the following command from `./admin-cli`
    
    ```
    $ npm run mla -- migrate --file=./firebase/migrations/397/set-labs-hidden.js --cfg.template=SOME_TEMPLATE
    ```
    
    Ideally, add the `--dry-run` option to double-check.

---
+++ firebase/migrations/397/set-labs-hidden.js
@@ -0,0 +1,42 @@
+const chalk = require('chalk');
+
+module.exports = function (db, argv) {
+
+  const delta = {};
+
+  if (argv['dry-run']) {
+    console.log(chalk.blue('INFO: Performing migration as dry-run'));
+  }
+
+  console.log(chalk.blue('Fetching labs without `hidden` field...'));
+
+  return db.ref('/labs')
+    .orderByChild('common/hidden').equalTo(null)
+    .once('value')
+    .then(snapshot => snapshot.val())
+    .then(labs => Object.entries(labs || {}))
+    .then(entries => {
+      if (entries.length === 0) {
+        console.log(chalk.blue('No labs without `hidden` field found. Migration done.'));
+      } else {
+        console.log(chalk.blue(`Found ${entries.length} labs`));
+      }
+      return entries.map(entry => entry[1])
+    })
+    .then(labs => {
+      labs.forEach(lab => {
+        console.log(chalk.blue(`Setting \`hidden\` field to \`false\` for lab: ${lab.common.id}`));
+        delta[`/labs/${lab.common.id}/common/hidden`] = false;
+
+        console.log(chalk.blue(`Adding lab: ${lab.common.id} to visible labs for user: ${lab.common.user_id}`));
+        delta[`/idx/user_visible_labs/${lab.common.user_id}/${lab.id}`] = true;
+      });
+    })
+    .then(_ => {
+      if (!argv['dry-run']) {
+        console.log(chalk.blue('Updating database...'));
+        return db.ref().update(delta);
+      }
+      console.log(chalk.blue('Migration was done as dry-run. No database updates have been performed.'));
+    });
+}
