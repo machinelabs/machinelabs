1bdc46acfaf393fc9376bd2129cec39e0e271284
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-08-23 16:59:51 +0200

    feat: use log4js for logging
    With this change we are using the log4js library
    to do all logging.
    
    There are a couple of things to highlight.
    
    1. We can just continue to use console.log,
       console.error etc.
    
    2. Slack logging will automatically enable when
       the env var MACHINELABS_SLACK_TOKEN is set
    
    3. For staging and production there are two
       different rooms configured
    
       log-staging-all   // all logs all from staging
       log-staging-error // only errors from staging
       log-production-all // all logs from production
       log-production-error // only errors from production
    
    Fixes #331

--- server/src/util/config.ts
+++ server/src/util/config.ts
@@ -7,11 +7,17 @@ export class Config {
   static readonly ENV_PRIVATE_KEY = 'MACHINELABS_FB_PRIVATE_KEY';
   static readonly ENV_CLIENT_EMAIL = 'MACHINELABS_FB_CLIENT_EMAIL';
 
+  static readonly ENV_SLACK_TOKEN = 'MACHINELABS_SLACK_TOKEN';
+
   static getEnv (name: string) {
     if (!process.env[name]) {
       console.log(`UNABLE TO LOCATE ENVIRONMENT VARIABLE: ${name}`);
       throw new Error(`Unable to locate environment variable: ${name}`);
     }
+    return process.env[name];
+  }
+
+  static tryGetEnv(name: string) {
     return process.env[name];
   }
 }
