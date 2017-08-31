3b7981f72070d4df0db6d186af9d2e7701935aa8
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-08-31 16:55:34 +0200

    chore(server): adds env file for second server

---
+++ server/src/environments/environment.production.2.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyDB6mmk8CDyy9D6DpYQgzLd3-wwY5WDSEc',
+    authDomain: 'machinelabs-production.firebaseapp.com',
+    databaseURL: 'https://machinelabs-production.firebaseio.com',
+    projectId: 'machinelabs-production',
+    storageBucket: 'machinelabs-production.appspot.com',
+    messagingSenderId: '273257478502'
+  },
+  serverId: 'stephanskirchen',
+  slackLogging: {
+    allChannel: 'log-production-all',
+    errorChannel: 'log-production-error'
+  }
+};
