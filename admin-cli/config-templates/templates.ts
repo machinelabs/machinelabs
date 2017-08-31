4d0de2814e6701520be8ff7548325456795e3df0
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-08-31 21:41:10 +0200

    refactor(admin-cli): deduplicate templates

--- admin-cli/config-templates/templates.ts
+++ admin-cli/config-templates/templates.ts
@@ -1,66 +1,56 @@
+const stagingTemplate = {
+  server: {
+    name: 'ahlem',
+    zone: 'europe-west1-c',
+    env: 'staging'
+  },
+  client: {
+    env: 'staging'
+  },
+  firebase: {
+    databaseUrl: 'https://machinelabs-a73cd.firebaseio.com',
+    privateKeyEnv: 'MACHINELABS_STAGING_FB_PRIVATE_KEY',
+    clientEmailEnv: 'MACHINELABS_STAGING_FB_CLIENT_EMAIL'
+  },
+  googleProjectId: 'machinelabs-a73cd'
+};
+
+const productionTemplate = {
+  server: {
+    name: 'hainholz',
+    zone: 'us-east1-d',
+    env: 'production'
+  },
+  client: {
+    env: 'production'
+  },
+  firebase: {
+    databaseUrl: 'https://machinelabs-production.firebaseio.com',
+    privateKeyEnv: 'MACHINELABS_PRODUCTION_FB_PRIVATE_KEY',
+    clientEmailEnv: 'MACHINELABS_PRODUCTION_FB_CLIENT_EMAIL'
+  },
+  googleProjectId: 'machinelabs-production'
+};
+
 export const templates = {
-  staging: {
-    server: {
-      name: 'ahlem',
-      zone: 'europe-west1-c',
-      env: 'staging'
-    },
-    client: {
-      env: 'staging'
-    },
-    firebase: {
-      databaseUrl: 'https://machinelabs-a73cd.firebaseio.com',
-      privateKeyEnv: 'MACHINELABS_STAGING_FB_PRIVATE_KEY',
-      clientEmailEnv: 'MACHINELABS_STAGING_FB_CLIENT_EMAIL'
-    },
-    googleProjectId: 'machinelabs-a73cd'
-  },
+  staging: stagingTemplate,
   staging2: {
-    server: {
-      name: 'list',
-      zone: 'europe-west1-c',
-      env: 'staging2'
-    },
-    client: {
-      env: 'staging'
-    },
-    firebase: {
-      databaseUrl: 'https://machinelabs-a73cd.firebaseio.com',
-      privateKeyEnv: 'MACHINELABS_STAGING_FB_PRIVATE_KEY',
-      clientEmailEnv: 'MACHINELABS_STAGING_FB_CLIENT_EMAIL'
-    },
-    googleProjectId: 'machinelabs-a73cd'
+    ...stagingTemplate, ...{
+      server: {
+        name: 'list',
+        zone: 'europe-west1-c',
+        env: 'staging2'
+      }
+    }
   },
-  production: {
-    server: {
-      name: 'hainholz',
-      zone: 'us-east1-d',
-      env: 'production'
-    },
-    client: {
-      env: 'production'
-    },
-    firebase: {
-      databaseUrl: 'https://machinelabs-production.firebaseio.com',
-      privateKeyEnv: 'MACHINELABS_PRODUCTION_FB_PRIVATE_KEY',
-      clientEmailEnv: 'MACHINELABS_PRODUCTION_FB_CLIENT_EMAIL'
-    },
-    googleProjectId: 'machinelabs-production'
-  },
+  production: productionTemplate,
   production2: {
-    server: {
-      name: 'stephanskirchen',
-      zone: 'europe-west1-c',
-      env: 'production2'
-    },
-    client: {
-      env: 'production'
-    },
-    firebase: {
-      databaseUrl: 'https://machinelabs-production.firebaseio.com',
-      privateKeyEnv: 'MACHINELABS_PRODUCTION_FB_PRIVATE_KEY',
-      clientEmailEnv: 'MACHINELABS_PRODUCTION_FB_CLIENT_EMAIL'
-    },
-    googleProjectId: 'machinelabs-production'
+    ...productionTemplate, ...{
+      server: {
+        name: 'stephanskirchen',
+        zone: 'europe-west1-c',
+        env: 'production2'
+      }
+    }
   }
 };
