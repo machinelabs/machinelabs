8af6181f1e45ad16dc1bf9162605bef5b17a83bd
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-09-04 13:45:01 +0200

    feat(client): add OutputFilesService

---
+++ client/src/app/output-files.service.ts
@@ -0,0 +1,36 @@
+import * as firebase from 'firebase';
+
+import { Injectable } from '@angular/core';
+import { DbRefBuilder } from './firebase/db-ref-builder';
+import { AuthService } from './auth';
+import { OutputFile } from './models/output-file';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/mergeMap';
+
+const fromSnapshot = snapshot => snapshot.val();
+
+@Injectable()
+export class OutputFilesService {
+
+  constructor(
+    private db: DbRefBuilder,
+    private authService: AuthService
+  ) {}
+
+  observeOutputFilesFromExecution(executionId: string): Observable<OutputFile> {
+    return this.authService
+               .requireAuthOnce()
+               .switchMap(_ => this.db.executionOutputFilesRef(executionId).childAdded().map(fromSnapshot))
+               .flatMap(file => this.getDownloadUrlForPath(file.path).map(download_url => ({...file, download_url})));
+  }
+
+  getDownloadUrlForPath(path: string) {
+    return Observable.fromPromise(firebase.storage()
+                                          .ref(path)
+                                          .getDownloadURL());
+  }
+}
+
