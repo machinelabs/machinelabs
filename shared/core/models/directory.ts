a2002005550f65fd20fe4b897cb04cbb9899b5fc
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-09-11 20:55:42 +0200

    feat(@machinelabs/core): support for writing nested directories
    This adds fs tools that support writing nested
    directories.
    
    Relates to #502

---
+++ shared/core/models/directory.ts
@@ -0,0 +1,19 @@
+export interface File {
+  name: string;
+  content: string;
+}
+
+export interface Directory {
+  name: string,
+  contents: Array<File|Directory>
+}
+
+export type LabDirectory = Array<File|Directory>
+
+export const instanceOfFile = (object: any): object is File => {
+  return object && 'content' in object;
+};
+
+export const instanceOfDirectory = (object: any): object is File => {
+  return object && 'contents' in object;
+};
