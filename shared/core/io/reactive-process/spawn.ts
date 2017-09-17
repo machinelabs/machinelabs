b7a64a5aed971c3e2ba2088719fc261fd5df1bc0
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-09-17 22:28:52 +0200

    chore(@machinelabs/core): improve ergonomics with type aliases

--- shared/core/io/reactive-process/spawn.ts
+++ shared/core/io/reactive-process/spawn.ts
@@ -1,7 +1,10 @@
 import { SpawnOptions, spawn as _spawn } from 'child_process';
 import { Observable } from '@reactivex/rxjs';
 import { toObservableProcess } from './observable-process';
-import { OutputType } from './process-stream-data';
+import { OutputType, ProcessStreamData } from './process-stream-data';
+
+export type SpawnFn = (command: string, args?: Array<string>, options?: SpawnOptions) => Observable<ProcessStreamData>;
+export type SpawnShellFn = (command: string, args?: Array<string>) => Observable<ProcessStreamData>;
 
 export const spawn = (command: string, args?: Array<string>, options?: SpawnOptions) => {
   return Observable.defer(() => {
