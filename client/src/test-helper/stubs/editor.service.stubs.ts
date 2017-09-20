75c1901c7c0f64b6dfcf02952c004e2ebd43b43b
Pascal Precht <pascal.precht@googlemail.com>
2017-09-20 15:40:26 +0200

    feat: introduce editorServiceStub and reexport stubs from /stubs

---
+++ client/src/test-helper/stubs/editor.service.stubs.ts
@@ -0,0 +1,16 @@
+import { LAB_STUB } from './lab.stubs';
+import { File, Directory, LabDirectory } from '../../app/models/lab';
+
+export const EDITOR_SERVICE_STUB = {
+  lab: Object.assign({}, LAB_STUB),
+  openFile: (file: File) => {},
+  deleteFileFromDirectory: (file: File, directory: Directory) => {
+    directory.contents.splice(directory.contents.indexOf(file), 1);
+  },
+  updateFileInDirectory: (file: File, newFile: File, directory: Directory) => {
+    const index = directory.contents.findIndex(f => f.name === file.name);
+    if (index !== -1) {
+      directory.contents[index] = newFile;
+    }
+  }
+}
