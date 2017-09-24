18595fcd5d97e8ab0abebb1155b12f5d818d8c36
Pascal Precht <pascal.precht@googlemail.com>
2017-09-24 11:17:52 +0200

    feat(EditorService): introduce methods to delete and update files

--- client/src/app/editor/editor.service.ts
+++ client/src/app/editor/editor.service.ts
@@ -123,6 +123,17 @@ export class EditorService {
     this.initActiveFile();
   }
 
+  deleteFromDirectory(fileOrDirectory: File|Directory, directory: Directory) {
+    directory.contents.splice(directory.contents.indexOf(fileOrDirectory), 1);
+  }
+
+  updateFileInDirectory(file: File, newFile: File, directory: Directory) {
+    const index = directory.contents.findIndex(f => f.name === file.name);
+    if (index !== -1) {
+      directory.contents[index] = newFile;
+    }
+  }
+
   listen(executionId: string) {
     return this.rleService.listen(executionId);
   }
