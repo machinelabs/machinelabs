79d2db3f4e4c6b56c354d181dde1fa999b6243cf
Pascal Precht <pascal.precht@googlemail.com>
2017-09-28 15:46:24 +0200

    refactor: move FileTree related methods in new FileTreeService

--- client/src/app/editor/editor.module.ts
+++ client/src/app/editor/editor.module.ts
@@ -12,6 +12,7 @@ import { EditorLayoutPanelCtaBarComponent } from './layout/editor-layout-panel-c
 import { EditorLayoutFooterComponent } from './layout/editor-layout-footer.component';
 
 import { FileTreeComponent } from './file-tree/file-tree.component';
+import { FileTreeService } from './file-tree/file-tree.service';
 import { FileListComponent } from './file-list/file-list.component';
 import { NameDialogComponent } from './name-dialog/name-dialog.component';
 import { AceEditorComponent } from './ace-editor/ace-editor.component';
@@ -52,6 +53,7 @@ import { FILE_PREVIEW_DIALOG_SCROLL_STRATEGY_PROVIDER } from './file-preview/fil
   ],
   providers: [
     EditorService,
+    FileTreeService,
     RemoteLabExecService,
     EditorSnackbarService,
     FilePreviewDialogService,
