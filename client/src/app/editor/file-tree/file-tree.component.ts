6c4919df4e6aa955efe308936422fdf2cca0e8ab
Pascal Precht <pascal.precht@googlemail.com>
2017-10-03 11:30:23 +0200

    refactor: move getMainFile method into LabDirectoryService

--- client/src/app/editor/file-tree/file-tree.component.ts
+++ client/src/app/editor/file-tree/file-tree.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { File, Directory, LabDirectory } from '@machinelabs/core/models/directory';
-import { getMainFile } from '../util/file-tree-helper';
 
 import { EditorService } from '../editor.service';
 
