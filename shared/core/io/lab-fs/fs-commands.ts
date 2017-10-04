10e34db00b851d0c64678c818b0b1ba22ef03712
Pascal Precht <pascal.precht@googlemail.com>
2017-10-04 11:29:53 +0200

    fix(@machinelabs/core): fix linting issues

--- shared/core/io/lab-fs/fs-commands.ts
+++ shared/core/io/lab-fs/fs-commands.ts
@@ -1,21 +1,21 @@
 import { Directory, File, instanceOfFile, instanceOfDirectory, LabDirectory } from '../../models/directory';
 
-const concatIfNotEmpty = (cmd:string) => cmd.length > 0 ? ` && ${cmd}` : '';
+const concatIfNotEmpty = (cmd: string) => cmd.length > 0 ? ` && ${cmd}` : '';
 
 // ATTENTION: The formatting is important here. We have to use a Here Doc because of multi line strings
 // http://stackoverflow.com/questions/2953081/how-can-i-write-a-here-doc-to-a-file-in-bash-script/2954835#2954835
-const createWriteFileCmd = (file:File) => `{ cat <<'EOL' > ${file.name}
+const createWriteFileCmd = (file: File) => `{ cat <<'EOL' > ${file.name}
 ${file.content}
 EOL
 }`;
 
 export const createWriteLabDirectoryCmd = (labDirectory: LabDirectory) =>
-                                          createWriteDirectoryCmd({ contents: labDirectory, name: '' }, true)
+                                          createWriteDirectoryCmd({ contents: labDirectory, name: '' }, true);
 
 export const createWriteDirectoryCmd = (directory: Directory, skipRoot = false): string => {
-  let commands:Array<string> = [];
+  let commands: Array<string> = [];
   directory.contents.forEach(fileOrDir => {
-    if (instanceOfFile(fileOrDir)){
+    if (instanceOfFile(fileOrDir)) {
       commands.push(createWriteFileCmd(fileOrDir));
     } else if (instanceOfDirectory(fileOrDir)) {
       commands.push(createWriteDirectoryCmd(fileOrDir));
