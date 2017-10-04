7375f4e48b66e2afdf64f70021243ed38215f0c3
Pascal Precht <pascal.precht@googlemail.com>
2017-10-04 11:29:53 +0200

    fix(@machinelabs/core): fix linting issues

--- shared/core/io/lab-fs/fs.ts
+++ shared/core/io/lab-fs/fs.ts
@@ -5,6 +5,11 @@ import { Directory, LabDirectory, File } from '../../models/directory';
 import { createWriteDirectoryCmd, createWriteLabDirectoryCmd } from './fs-commands';
 
 
-export const writeDirectory = (directory: Directory, skipRoot?: boolean) => spawnShell(createWriteDirectoryCmd(directory, skipRoot));
+export const writeDirectory = (directory: Directory, skipRoot?: boolean) => {
+  return spawnShell(createWriteDirectoryCmd(directory, skipRoot));
+};
 
-export const writeLabDirectory = (labDirectory: LabDirectory) => spawnShell(createWriteLabDirectoryCmd(labDirectory));
+export const writeLabDirectory = (labDirectory: LabDirectory) => {
+  return spawnShell(createWriteLabDirectoryCmd(labDirectory));
+};
+
