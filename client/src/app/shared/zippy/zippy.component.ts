d54443e1c304863758edb3f229212575b45d953c
Pascal Precht <pascal.precht@googlemail.com>
2017-10-04 13:04:27 +0200

    WIP SAVEPOINT

---
+++ client/src/app/shared/zippy/zippy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ml-zippy',
+  templateUrl: './zippy.component.html',
+  styleUrls: ['./zippy.component.css']
+})
+export class ZippyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
