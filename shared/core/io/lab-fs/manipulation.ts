7a794a365cf4cc4bcb8ba584509f6b9e6532a97b
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-10-10 18:36:06 +0200

    refactor: move methods from client to core

---
+++ shared/core/io/lab-fs/manipulation.ts
@@ -0,0 +1,12 @@
+import { Directory, File } from '../../models/directory';
+
+export const deleteFromDirectory = (fileOrDirectory: File|Directory, directory: Directory) => {
+  directory.contents.splice(directory.contents.indexOf(fileOrDirectory), 1);
+};
+
+export const updateFileInDirectory = (file: File, newFile: File, directory: Directory) => {
+  const index = directory.contents.findIndex(f => f.name === file.name);
+  if (index !== -1) {
+    directory.contents[index] = newFile;
+  }
+};
---
+++ shared/core/io/lab-fs/navigation.ts
@@ -0,0 +1,50 @@
+import { Directory, LabDirectory, File, instanceOfFile, instanceOfDirectory } from '../../models/directory';
+
+const findFile = (name: string) => {
+  return (fileOrDirectory: File | Directory) => {
+    return fileOrDirectory.name === name && instanceOfFile(fileOrDirectory);
+  };
+};
+
+const findDirectory = (name: string) => {
+  return (fileOrDirectory: File | Directory) => {
+    return fileOrDirectory.name === name && instanceOfDirectory(fileOrDirectory);
+  };
+};
+
+export const normalizePathSegments = (path: string) => {
+  let segments = path.split('/');
+  if (segments[0] === '') {
+    segments.splice(0, 1);
+  }
+  return segments;
+};
+
+export const getFileFromPath = (path: string, directory: LabDirectory) => {
+  let pathSegments = normalizePathSegments(path);
+  let currentDirectory = directory;
+  let file = null;
+
+  for (let i = 0; i < pathSegments.length; i++) {
+    let name = pathSegments[i];
+    let lookingForDirectory = i !== pathSegments.length - 1;
+
+    let fileOrDirectory = currentDirectory
+        .find(lookingForDirectory ? findDirectory(name) : findFile(name));
+
+    if (!fileOrDirectory) {
+      break;
+    }
+
+    if (lookingForDirectory) {
+      if (instanceOfDirectory(fileOrDirectory)) {
+        currentDirectory = fileOrDirectory.contents;
+      }
+    } else {
+      if (instanceOfFile(fileOrDirectory)) {
+        file = fileOrDirectory;
+      }
+    }
+  }
+  return file;
+};
