3938e7f2afb4f69fe69d1a8c49eae656d1a43f2a
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-10-13 16:09:02 +0200

    WIP

---
+++ admin-cli/lib/deploy-rest-api.ts
@@ -0,0 +1,34 @@
+import * as chalk from 'chalk';
+import { Observable } from '@reactivex/rxjs';
+import { OutputType, stdout, spawnShell } from '@machinelabs/core';
+import { factory } from './execute';
+import * as fs from 'fs';
+import * as pty from 'node-pty';
+
+import { isRootDir } from './is-root-dir';
+import { failWith } from './fail-with';
+import { processify } from './processify';
+
+export function deployRestApi(project, env) {
+  if (!isRootDir()) {
+    failWith('Command needs to be run from root dir');
+  }
+
+  return Observable.concat(
+    stdout(chalk.green('Deploying REST API')),
+
+    spawnShell(`(cd ./rest-api && npm run node_modules && npm run build -- --env=${env})`),
+
+    processify(() => {
+      if (!fs.existsSync('./rest-api/dist')) {
+        console.log(chalk.red('Dist does not exist. Aborting'));
+        process.exit(1);
+      }
+    }),
+
+    // tslint:disable-next-line
+    spawnShell(`(cd ./rest-api && gcloud app deploy --quiet --project "${project}")`),
+
+    stdout(chalk.green('REST API successfully deployed'))
+  );
+}
