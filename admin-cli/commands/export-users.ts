d4f85ed0b54dc39df124631ea9ed794f49096115
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-10-13 11:54:33 +0200

    feat(admin-cli): adds export-users command
    This command generates a csv of all onboarded
    users. The csv can be imported to mail chimp
    so that we have a better connection to our
    user base.

---
+++ admin-cli/commands/export-users.ts
@@ -0,0 +1,50 @@
+import { createDb } from '../lib/create-db';
+import { getEnv } from '../lib/get-env';
+import { ObservableDbRef } from '@machinelabs/core';
+import { Observable } from '@reactivex/rxjs';
+import { factory } from '../lib/execute';
+
+let execute = factory({displayErrors: true});
+
+const hasArgsForOnboard = argv => argv.cfg &&
+                                  argv.cfg.firebase.privateKeyEnv &&
+                                  argv.cfg.firebase.clientEmailEnv &&
+                                  argv.cfg.firebase.databaseUrl;
+
+const exportUsers = (argv) => {
+  if (hasArgsForOnboard(argv)) {
+    let fbPrivateKey = getEnv(argv.cfg.firebase.privateKeyEnv);
+    let fbClientEmail = getEnv(argv.cfg.firebase.clientEmailEnv);
+    let fbDatabaseUrl = argv.cfg.firebase.databaseUrl;
+
+    let db = createDb(fbPrivateKey, fbClientEmail, fbDatabaseUrl);
+
+    console.log('Exporting users. Abort any time.')
+    execute('rm -rf user_export.csv');
+    execute('touch user_export.csv');
+
+    new ObservableDbRef(db.ref('/users'))
+      .childAdded()
+      .map(snapshot => snapshot.val())
+      .filter(val => !!val && val.plan)
+      .subscribe(val => {
+        console.log(`Exporting ${val.common.displayName}\t${val.common.email}`)
+        execute(`echo "${val.common.displayName}\t${val.common.email}" >> user_export.csv`);
+      }, e => {
+        console.error(e);
+      },() => {
+        console.log('Finished');
+      });
+  }
+}
+
+const check = argv => {
+  if (argv._.includes('export-users') && !hasArgsForOnboard(argv)) {
+    throw new Error('Command needs `cfg.firebase.privateKeyEnv`, `cfg.firebase.clientEmailEnv` and `cfg.firebase.databaseUrl`');
+  }
+};
+
+export const exportUsersCommand = {
+  run: exportUsers,
+  check: check
+};
