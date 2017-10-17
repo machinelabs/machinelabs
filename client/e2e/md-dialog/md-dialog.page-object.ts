a74017189fcd5a39397807dcdbb5ee6d9c8a2083
Pascal Precht <pascal.precht@googlemail.com>
2017-10-17 14:48:42 +0200

    chore: upgrade Angular Material and CDK to 2.0.0-beta.12

--- client/e2e/md-dialog/md-dialog.page-object.ts
+++ client/e2e/md-dialog/md-dialog.page-object.ts
@@ -2,7 +2,7 @@ import { element, by, ElementFinder } from 'protractor';
 
 export class MdDialogPageObject {
   overlayContainer: ElementFinder = element(by.css('.cdk-overlay-container'));
-  dialogContainer: ElementFinder = this.overlayContainer.element(by.tagName('md-dialog-container'));
+  dialogContainer: ElementFinder = this.overlayContainer.element(by.tagName('mat-dialog-container'));
 
   getDialogComponent(tagName: string): ElementFinder {
     return this.dialogContainer.element(by.tagName(tagName));
