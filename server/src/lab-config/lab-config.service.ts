44929f487e55ab152b99529ef30ac80349832cbe
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-10-25 18:55:44 +0200

    refactor(server): improve configuration handling
    This change improves the handling of configuration
    files in the following ways:
    
    1. Previously the InternalLabConfiguration inherited
       from the PublicLabConfiguration. Even though we
       were already careful to validate everything,
       this could quickly become a security issue if
       user provided keys accidentially make their way
       through to the InternalLabConfiguration.
    
       With this change, there is no direct inheritance
       anymore. There's an explicit transformation for
       every single property.
    
    2. All transformations are done in the LabConfigurationService
       which comes at the benefit that:
    
       A. The LabConfigResolver really only resolves a config
       B. The HasValidConfig rule really only rejects and reports
          errors that were found during the transformation.
    
       The LabConfigurationService is the single point for
       performing the actual transformations.
    
    3. The UI provides richer feedback for each and every
       thing that may fail when parsing the ml.yaml file.

--- server/src/lab-config/lab-config.service.ts
+++ server/src/lab-config/lab-config.service.ts
@@ -1,14 +1,21 @@
+import { Observable } from '@reactivex/rxjs';
 import { Lab, File, instanceOfFile, MountOption } from '@machinelabs/models';
 import { PublicLabConfiguration, InternalLabConfiguration, ScriptParameter } from '../models/lab-configuration';
 import { safeLoad} from 'js-yaml';
 import isString = require('lodash.isstring');
 import isObject = require('lodash.isobject');
+import { DockerImageService } from '../docker-image.service';
+import { MountService } from '../mounts/mount.service';
+import { mute } from '../rx/mute';
 
 const CONFIG_FILE_NAME = 'ml.yaml';
 
 export class LabConfigService {
 
-  readConfig(lab: Lab): PublicLabConfiguration {
+  constructor(private dockerImageService: DockerImageService,
+              private mountService: MountService) {}
+
+  readPublicConfig(lab: Lab): PublicLabConfiguration {
 
     let configFile = this.getMlYaml(lab);
 
@@ -19,11 +26,35 @@ export class LabConfigService {
     try {
       let config = safeLoad(configFile.content);
 
-      let actualConfig = Object.assign(new PublicLabConfiguration(), config);
-      return this.isValidPublicConfig(actualConfig) ? actualConfig : null;
+      return Object.assign(new PublicLabConfiguration(), config);
     } catch (error) {
       return null;
     }
+  }
+
+  getInternalConfig(userId: string, lab: Lab) {
+    return this.toInternalConfig(userId, this.readPublicConfig(lab));
+  }
+
+  toInternalConfig(userId: string, publicConfig: PublicLabConfiguration) {
+    let config = new InternalLabConfiguration();
+
+    if (!publicConfig) {
+      config.errors.push('Could not parse file');
+      return Observable.of(config);
+    }
+
+    config.inputs = publicConfig.inputs;
+    config.parameters = publicConfig.parameters;
+    config.imageWithDigest = this.dockerImageService.getImageNameWithDigest(publicConfig.dockerImageId);
+
+    return this.mountService.validateMounts(userId, publicConfig.mounts)
+    .do(validated => config.errors.push(...validated.errors))
+    .do(validated => config.mountPoints = validated.mountPoints)
+    .map(_ => config)
+    .map(this.reportError('Invalid parameters', this.hasValidParameters))
+    .map(this.reportError('Unknown dockerImageId', this.isValidImage))
+    .map(this.reportError('Invalid inputs', this.hasValidInputs));
   }
 
   private getMlYaml(lab: Lab) {
@@ -33,19 +64,28 @@ export class LabConfigService {
     return instanceOfFile(file) ? file : null;
   }
 
-  public isValidPublicConfig(config: PublicLabConfiguration) {
-    // The dockerImageId is the only mandatory property on the public config
-    return !!config.dockerImageId;
+  private reportError(error: string, validate: (config: InternalLabConfiguration) => boolean) {
+    return (config: InternalLabConfiguration) => {
+      let valid = validate(config);
+
+      if (!valid) {
+        config.errors.push(error);
+      }
+
+      return config;
+    };
   }
 
-  public isValidInternalConfig(config: InternalLabConfiguration) {
-    return isString(config.imageWithDigest) && config.imageWithDigest.length > 0 &&
-      Array.isArray(config.inputs) && this.hasValidParameters(config);
+  private hasValidInputs(config: InternalLabConfiguration) {
+    return Array.isArray(config.inputs) && config.inputs.every(param => isObject(param) && !!param['name'] && !!param['url']);
+  }
+
+  private isValidImage(config: InternalLabConfiguration) {
+    return isString(config.imageWithDigest) && config.imageWithDigest.length > 0;
   }
 
   private hasValidParameters(config: InternalLabConfiguration) {
-    return Array.isArray(config.parameters) &&
-      config.parameters.every(param => isObject(param) && !!param['pass-as']);
+    return Array.isArray(config.parameters) && config.parameters.every(param => isObject(param) && !!param['pass-as']);
   }
 
 }
