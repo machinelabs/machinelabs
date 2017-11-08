f6f14b97129a33e7d02a30543dde388d05171c99
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-11-08 15:15:11 +0100

    feat(firebase/functions): assign server based on ml.yaml setting

--- firebase/functions/src/invocation-processor/server-resolver/resolve-strategies.ts
+++ firebase/functions/src/invocation-processor/server-resolver/resolve-strategies.ts
@@ -1,16 +1,34 @@
 import * as toArray from 'lodash.toarray';
 import * as sample from 'lodash.sample';
+import * as isString from 'lodash.isstring';
 import { database } from '../database';
 import { Invocation } from '../../models/invocation';
+import { readLabConfig } from '../util/lab-config-helper';
+import { HardwareType } from '../../models/server';
 
-const DEFAULT_HARDWARE_TYPE = 'economy';
+
+const DEFAULT_HARDWARE_TYPE = HardwareType.CPU;
 
 export function getServerIdForHardwareType(invocation: Invocation) {
+
+  let config = readLabConfig(invocation);
+
+  let specifiedHardwareType = config && isString(config.hardwareType) ? config.hardwareType.toLowerCase() : DEFAULT_HARDWARE_TYPE;
+
+  console.log(`User specified hardware type ${specifiedHardwareType}`);
+
+  let hardwareTypeExists = !!Object.keys(HardwareType).map(key => HardwareType[key]).find(val => val === specifiedHardwareType);
+
+  console.log(`User specified hardware does ${hardwareTypeExists ? 'exist' : 'not exist'}`);
+
+  let hardwareType = hardwareTypeExists ? specifiedHardwareType : DEFAULT_HARDWARE_TYPE;
+
+  console.log(`Trying to assign server of hardware type ${hardwareType}`);
 
   return database
        .ref('servers')
        .orderByChild('hardware_type')
-       .equalTo(DEFAULT_HARDWARE_TYPE)
+       .equalTo(hardwareType)
        .once('value')
        .then(snapshot => snapshot.val())
        // pick a random server
---
+++ firebase/functions/src/models/directory.ts
@@ -0,0 +1,32 @@
+// DO NOT EDIT. COPY FROM @machinelabs/models instead
+
+export interface DirectoryClientState {
+  collapsed?: boolean;
+  selected?: boolean;
+}
+
+export interface File {
+  name: string;
+  content: string;
+  clientState?: DirectoryClientState;
+}
+
+export interface Directory {
+  name: string;
+  contents: Array<File|Directory>;
+  clientState?: DirectoryClientState;
+}
+
+export type LabDirectory = Array<File|Directory>;
+
+export const isLabDirectory = (object: any): object is LabDirectory => {
+  return Array.isArray(object);
+};
+
+export const instanceOfFile = (object: any): object is File => {
+  return object && 'content' in object;
+};
+
+export const instanceOfDirectory = (object: any): object is Directory => {
+  return object && 'contents' in object;
+};
