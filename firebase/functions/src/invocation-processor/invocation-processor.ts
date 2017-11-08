1546570fe81c403578186f3cc084871f10bde2ff
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-11-08 15:15:03 +0100

    refactor(firebase/functions): improve general code quality

---
+++ firebase/functions/src/invocation-processor/invocation-processor.ts
@@ -0,0 +1,44 @@
+import { InvocationType, Invocation, InvocationWrapper } from '../models/invocation';
+import { ServerResolver } from './server-resolver/server-resolver';
+
+const DEFAULT_HARDWARE_TYPE = 'economy';
+
+export type getInvocationFn = (invocationId: string) => Promise<InvocationWrapper>;
+export type updateInvocationFn = (invocationWrapper: InvocationWrapper) => Promise<any>;
+
+export class InvocationProcessor {
+  constructor(private getInvocation: getInvocationFn,
+              private serverResolver: ServerResolver,
+              private updateInvocation: updateInvocationFn) {}
+
+  process(invocationId: string) {
+    console.log(`Processing invocation: ${invocationId}`);
+
+    if (!invocationId) {
+      console.log(`Invalid invocation id`);
+      return Promise.resolve(null);
+    }
+
+    return this.getInvocation(invocationId)
+               .then(invocationWrapper => this.serverResolver.getServer(invocationWrapper)
+                                                            .then(serverId => ({invocation: invocationWrapper, serverId: serverId })))
+               .then(val => {
+                 if (val && val.serverId) {
+                   this.setServer(val.invocation, val.serverId);
+                   return this.updateInvocation(val.invocation);
+                 }
+
+                 return val ? val.invocation : null;
+               });
+  }
+
+  private setServer(invocationWrapper, serverId) {
+    invocationWrapper.server = {
+      id: serverId,
+      [serverId]: {
+        timestamp: invocationWrapper.common.timestamp
+      }
+    };
+  }
+}
+
