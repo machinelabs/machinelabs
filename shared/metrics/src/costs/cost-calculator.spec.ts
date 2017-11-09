bcf1dfe053306e93645b60f09940624126225c73
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-11-09 13:07:21 +0100

    refactor: rename hardware types to CPU and GPU

--- shared/metrics/src/costs/cost-calculator.spec.ts
+++ shared/metrics/src/costs/cost-calculator.spec.ts
@@ -13,28 +13,28 @@ let mToS = (m: number) => m * 60;
 
 let generateExecutions = () => <Array<Execution>>[
   {
-    hardware_type: HardwareType.Economy,
+    hardware_type: HardwareType.CPU,
     started_at: Date.now() - mToMs(20),
     finished_at: Date.now() - mToMs(10),
   },
   {
-    hardware_type: HardwareType.Economy,
+    hardware_type: HardwareType.CPU,
     started_at: Date.now() - mToMs(30),
     finished_at: Date.now() - mToMs(15),
   },
   {
-    hardware_type: HardwareType.Economy,
+    hardware_type: HardwareType.CPU,
     started_at: Date.now() - mToMs(25),
     // This simulates an execution that is still running
     finished_at: null,
   },
   {
-    hardware_type: HardwareType.Premium,
+    hardware_type: HardwareType.GPU,
     started_at: Date.now() - mToMs(20),
     finished_at: Date.now() - mToMs(10),
   },
   {
-    hardware_type: HardwareType.Premium,
+    hardware_type: HardwareType.GPU,
     started_at: Date.now() - mToMs(30),
     finished_at: Date.now() - mToMs(15),
   }
@@ -50,17 +50,17 @@ describe('.calc()', () => {
 
     let expectedDurationEconomy = mToS(50);
     let expectedDurationPremium = mToS(25);
-    let expectedCostEconomy = +(expectedDurationEconomy * COST_PER_SECOND_PER_TYPE.get(HardwareType.Economy)).toFixed(2);
-    let expectedCostPremium = +(expectedDurationPremium * COST_PER_SECOND_PER_TYPE.get(HardwareType.Premium)).toFixed(2);
+    let expectedCostEconomy = +(expectedDurationEconomy * COST_PER_SECOND_PER_TYPE.get(HardwareType.CPU)).toFixed(2);
+    let expectedCostPremium = +(expectedDurationPremium * COST_PER_SECOND_PER_TYPE.get(HardwareType.GPU)).toFixed(2);
 
     let expectedTotalDuration = expectedDurationEconomy + expectedDurationPremium;
 
     calculator.calc(executions$)
               .subscribe(statistic => {
-                expect(statistic.secondsPerHardware.get(HardwareType.Economy)).toBe(expectedDurationEconomy);
-                expect(statistic.secondsPerHardware.get(HardwareType.Premium)).toBe(expectedDurationPremium);
-                expect(statistic.costPerHardware.get(HardwareType.Economy)).toBe(expectedCostEconomy);
-                expect(statistic.costPerHardware.get(HardwareType.Premium)).toBe(expectedCostPremium);
+                expect(statistic.secondsPerHardware.get(HardwareType.CPU)).toBe(expectedDurationEconomy);
+                expect(statistic.secondsPerHardware.get(HardwareType.GPU)).toBe(expectedDurationPremium);
+                expect(statistic.costPerHardware.get(HardwareType.CPU)).toBe(expectedCostEconomy);
+                expect(statistic.costPerHardware.get(HardwareType.GPU)).toBe(expectedCostPremium);
                 expect(statistic.totalSeconds).toBe(expectedTotalDuration);
                 expect(statistic.totalCost).toBe(expectedCostEconomy + expectedCostPremium);
                 done();
--- shared/metrics/src/costs/costs.ts
+++ shared/metrics/src/costs/costs.ts
@@ -1,6 +1,6 @@
 import { HardwareType } from '@machinelabs/models';
 
 export const COST_PER_SECOND_PER_TYPE = new Map<HardwareType, number>();
-COST_PER_SECOND_PER_TYPE.set(HardwareType.Economy, 0.000012);
-COST_PER_SECOND_PER_TYPE.set(HardwareType.Premium, 0.00012);
+COST_PER_SECOND_PER_TYPE.set(HardwareType.CPU, 0.000012);
+COST_PER_SECOND_PER_TYPE.set(HardwareType.GPU, 0.00012);
 
