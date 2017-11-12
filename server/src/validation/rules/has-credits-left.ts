202927a41d33c48a1b89088d4e06afd356885f97
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-11-12 18:42:04 +0100

    feat: max cap GPU hours

--- server/src/validation/rules/has-credits-left.ts
+++ server/src/validation/rules/has-credits-left.ts
@@ -1,10 +1,12 @@
 import { Observable } from '@reactivex/rxjs';
 import { ValidationRule } from './rule';
-import { Invocation, ExecutionRejectionInfo, ExecutionRejectionReason } from '@machinelabs/models';
+import { Invocation, HardwareType, ExecutionRejectionInfo, ExecutionRejectionReason } from '@machinelabs/models';
 import { ValidationResult } from '../validation-result';
 import { UserResolver } from '../resolver/user-resolver';
 import { UsageStatisticResolver } from '../resolver/usage-statistic-resolver';
+import { LabConfigResolver } from '../resolver/lab-config-resolver';
 import { Plans } from '../../models/plans';
+import { UsageStatistic } from '@machinelabs/metrics';
 
 export class HasCreditsLeftRule implements ValidationRule {
 
@@ -14,13 +16,32 @@ export class HasCreditsLeftRule implements ValidationRule {
       throw new Error('Missing resoler: UsageStatisticResolver');
     }
 
-    return Observable.forkJoin(resolves.get(UsageStatisticResolver), resolves.get(UserResolver))
-      .map(([statistic, user]) => {
+    return Observable.forkJoin(resolves.get(UsageStatisticResolver),
+                               resolves.get(UserResolver),
+                               resolves.get(LabConfigResolver))
+      .map(([_statistic, user, config]) => {
         const isAdmin = user.plan.plan_id === Plans.Admin;
+        const hardwareType = config.hardwareType;
+        const statistic: UsageStatistic = _statistic;
 
-        return isAdmin ? true : !statistic || statistic.secondsLeft <= 0 ?
-          new ExecutionRejectionInfo(ExecutionRejectionReason.OutOfCredits, 'User is out of credits') :
-          true;
+        if (isAdmin) {
+          return true;
+        }
+
+        if (!statistic) {
+          return new ExecutionRejectionInfo(ExecutionRejectionReason.InternalError, 'Internal Error');
+        }
+
+        if ((hardwareType === HardwareType.CPU && statistic.cpuSecondsLeft <= 0)) {
+          return new ExecutionRejectionInfo(ExecutionRejectionReason.OutOfCpuCredits, 'User is out of CPU credits');
+        }
+
+
+        if (hardwareType === HardwareType.GPU && statistic.gpuSecondsLeft <= 0) {
+          return new ExecutionRejectionInfo(ExecutionRejectionReason.OutOfGpuCredits, 'User is out of GPU credits');
+        }
+
+        return true;
       });
   }
 }
