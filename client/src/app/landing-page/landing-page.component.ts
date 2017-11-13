74b2faa4e951766d68123ddf60c33e3287d2b57b
Pascal Precht <pascal.precht@googlemail.com>
2017-11-13 10:38:37 +0100

    refactor: overhaul landing page to make it look a little more organized

--- client/src/app/landing-page/landing-page.component.ts
+++ client/src/app/landing-page/landing-page.component.ts
@@ -1,30 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-
-import { LoginService } from '../login.service';
-import { UserService } from '../user/user.service';
-import { User } from '../models/user';
-import { map } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { MenuTriggerType } from '../toolbar/toolbar-menu/toolbar-menu.component';
 
 @Component({
   selector: 'ml-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent implements OnInit {
-  isLoggedIn$: Observable<boolean>;
-
-  constructor(
-    private loginService: LoginService,
-    private userService: UserService) { }
-
-  ngOnInit() {
-    this.isLoggedIn$ = this.userService.observeUserChanges().pipe(
-      map(user => !user.isAnonymous)
-    );
-  }
-
-  login() {
-    this.loginService.loginWithGitHub();
-  }
+export class LandingPageComponent {
+  MenuTriggerType = MenuTriggerType;
 }
