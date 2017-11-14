377784f27f31e578bf62c4035c98428a4b7aeb95
Pascal Precht <pascal.precht@googlemail.com>
2017-11-14 20:30:24 +0100

    fix(EmbeddedEditorView): use lettable operators
    Forgot one more place to use lettable operators. This broke our embedded editor view as discussed in #608
    
    Fixes #608

---
+++ client/e2e/embedded-editor-view/embedded-editor-view.e2e-spec.ts
@@ -0,0 +1,21 @@
+import { browser, protractor, element, by } from 'protractor';
+
+import { waitForContentReady } from './../utils';
+import { EmbeddedEditorViewPageObject } from './embedded-editor-view.page-object';
+
+describe('Editor-View Component', () => {
+
+  const embeddedEditorView = new EmbeddedEditorViewPageObject();
+
+  beforeEach(() => {
+    browser.get('/embedded');
+    waitForContentReady();
+  });
+
+  it('should active tab by query parameter', () => {
+    browser.get('/embedded?tab=console');
+    waitForContentReady();
+    expect(embeddedEditorView.activeTabLabel).toEqual('Console');
+  });
+});
+
---
+++ client/e2e/embedded-editor-view/embedded-editor-view.page-object.ts
@@ -0,0 +1,16 @@
+import {
+  protractor,
+  browser,
+  element,
+  by,
+  ElementFinder,
+  ElementArrayFinder,
+  promise
+} from 'protractor';
+
+export class EmbeddedEditorViewPageObject {
+
+  get activeTabLabel(): promise.Promise<string> {
+    return element(by.css('.mat-tab-link[ng-reflect-active="true"]')).getText();
+  }
+}
