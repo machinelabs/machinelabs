344491595979809988cc3123126e0d29147100fb
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-11-15 22:24:51 +0100

    chore: configure servers to pull images on startup

--- server/src/environments/environment.production.ts
+++ server/src/environments/environment.production.ts
@@ -8,6 +8,7 @@ export const environment = {
     messagingSenderId: '273257478502'
   },
   serverId: 'hainholz',
+  pullImages: true,
   rootMountPath: '/bucket',
   slackLogging: {
     allChannel: 'log-production-all',
--- server/src/environments/environment.staging-portimao.ts
+++ server/src/environments/environment.staging-portimao.ts
@@ -7,6 +7,7 @@ export const environment = {
     messagingSenderId: '351438476852'
   },
   serverId: 'portimao',
+  pullImages: true,
   rootMountPath: '/bucket',
   slackLogging: {
     allChannel: 'log-staging-all',
