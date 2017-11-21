776a38d65e41b038be30bc676a7ddc98ebf2007f
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-11-21 13:11:06 +0100

    fix: ensure user_executions idx is always updated
    Previously, the index wasn't updated in case
    finished_at would be missing but stopped_at
    or failed_at is set.
    
    Fixes #616

--- firebase/functions/src/user-execution-index-tools.spec.ts
+++ firebase/functions/src/user-execution-index-tools.spec.ts
@@ -1,5 +1,5 @@
 import 'jest';
-import { appendEntryToMonthIndex } from './user-execution-index-tools';
+import { appendEntryToMonthIndex, updateUserExecutions } from './user-execution-index-tools';
 
 describe('appendEntryToMonthIndex', () => {
   it('should create index tree of months', () => {
@@ -30,5 +30,96 @@ describe('appendEntryToMonthIndex', () => {
     };
 
     expect(index).toEqual(expected);
+  });
+});
+
+
+describe('updateUserExecutions', () => {
+  it('should add live entry', () => {
+
+    let delta = {};
+
+    let execution = {
+      id: '1',
+      user_id: 'foo',
+      started_at: 1000
+    };
+
+    let expected = {
+      '/idx/user_executions/foo/live/1': true
+    };
+
+    updateUserExecutions(null, execution, delta);
+
+    expect(delta).toEqual(expected);
+  });
+
+  it('should kill live entry and add 1970-Jan entry', () => {
+
+    let delta = {};
+
+    let execution = {
+      id: '1',
+      user_id: 'foo',
+      started_at: 1000,
+      finished_at: 1100
+    };
+
+    let expected = {
+      '/idx/user_executions/foo/1970/Jan/1': true,
+      '/idx/user_executions/foo/live/1': null,
+    };
+
+    updateUserExecutions(null, execution, delta);
+
+    expect(delta).toEqual(expected);
+  });
+
+  it('should kill live entry and add 2001-Sep entry (stopped_at)', () => {
+
+    let delta = {};
+
+    let execution = {
+      id: '1',
+      user_id: 'foo',
+      started_at:  1000000000000,
+      stopped_at: 1001000000000,
+      finished_at: 1002000000000
+    };
+
+    // we don't want to see Oct here because it should use the
+    // `stopped_at` rather than `finished_at` value
+    let expected = {
+      '/idx/user_executions/foo/2001/Sep/1': true,
+      '/idx/user_executions/foo/live/1': null,
+    };
+
+    updateUserExecutions(null, execution, delta);
+
+    expect(delta).toEqual(expected);
+  });
+
+  it('should kill live entry and add 2001-Sep entry (failed_at)', () => {
+
+    let delta = {};
+
+    let execution = {
+      id: '1',
+      user_id: 'foo',
+      started_at:  1000000000000,
+      failed_at: 1001000000000,
+      finished_at: 1002000000000
+    };
+
+    // we don't want to see Oct here because it should use the
+    // `failed_at` rather than `finished_at` value
+    let expected = {
+      '/idx/user_executions/foo/2001/Sep/1': true,
+      '/idx/user_executions/foo/live/1': null,
+    };
+
+    updateUserExecutions(null, execution, delta);
+
+    expect(delta).toEqual(expected);
   });
 });
