62d191b1e5bf9ca7f770ad486c0ca817bb5e7287
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-11-28 14:33:47 +0100

    refactor(admin-cli): make code more DRY

--- admin-cli/lib/print-statistic.ts
+++ admin-cli/lib/print-statistic.ts
@@ -1,26 +1,23 @@
 import { PlanCredits, HardwareType } from '@machinelabs/models';
 import { UsageStatistic } from '@machinelabs/metrics';
+import { prettyPrintDuration } from '../lib/pretty-print-duration';
 import * as padStart from 'lodash.padstart';
 import * as chalk from 'chalk';
-import * as prettyMs from 'pretty-ms';
-
-// prettyMs can't deal with negative durations and infinite numbers, so we have to help a little.
-let prettyDuration = (duration: number) => duration === Number.POSITIVE_INFINITY ? '∞' : duration > 0 ? prettyMs(duration) : '-' + prettyMs(duration * -1);
 
 export const printStatistic = (statistic: UsageStatistic) => {
   let credits = PlanCredits.get(statistic.planId);
         console.log(statistic);
         let hoursToMs = (hours: number) => hours * 60 * 60 * 1000;
 
-        let freeCpuTime = prettyDuration(hoursToMs(credits.cpuHoursPerMonth))
-        let actualCpuTime = prettyDuration(statistic.costReport.getSecondsPerHardware(HardwareType.CPU) * 1000);
+        let freeCpuTime = prettyPrintDuration(hoursToMs(credits.cpuHoursPerMonth))
+        let actualCpuTime = prettyPrintDuration(statistic.costReport.getSecondsPerHardware(HardwareType.CPU) * 1000);
         let tmpLeftCpuTime = statistic.cpuSecondsLeft * 1000;
-        let leftCpuTime = prettyDuration(statistic.cpuSecondsLeft * 1000);
+        let leftCpuTime = prettyPrintDuration(statistic.cpuSecondsLeft * 1000);
         let cpuCosts = statistic.costReport.getCostPerHardware(HardwareType.CPU);
 
-        let freeGpuTime = prettyDuration(hoursToMs(credits.gpuHoursPerMonth))
-        let actualGpuTime = prettyDuration(statistic.costReport.getSecondsPerHardware(HardwareType.GPU) * 1000);
-        let leftGpuTime = prettyDuration(statistic.gpuSecondsLeft * 1000);
+        let freeGpuTime = prettyPrintDuration(hoursToMs(credits.gpuHoursPerMonth))
+        let actualGpuTime = prettyPrintDuration(statistic.costReport.getSecondsPerHardware(HardwareType.GPU) * 1000);
+        let leftGpuTime = prettyPrintDuration(statistic.gpuSecondsLeft * 1000);
         let gpuCosts = statistic.costReport.getCostPerHardware(HardwareType.GPU);
 
         let pad = 17;
