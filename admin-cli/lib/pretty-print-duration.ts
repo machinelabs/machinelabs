3dc718aa5217bfa6c62bce2393d6b6a979d7b046
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-11-28 14:25:06 +0100

    feat(admin-cli): add new ls-live-executions command
    Fixes #599

---
+++ admin-cli/lib/pretty-print-duration.ts
@@ -0,0 +1,4 @@
+import * as prettyMs from 'pretty-ms';
+
+// prettyMs can't deal with negative durations and infinite numbers, so we have to help a little.
+export const prettyPrintDuration = (duration: number) => duration === Number.POSITIVE_INFINITY ? '∞' : duration > 0 ? prettyMs(duration) : '-' + prettyMs(duration * -1);
