d57b0ffb09334f86190d2cb720b124091b93795f
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-11-29 20:22:56 +0100

    feat: auto-assign 'beta' plan to new users

--- firebase/functions/src/post-user-create.ts
+++ firebase/functions/src/post-user-create.ts
@@ -1,7 +1,8 @@
 import * as functions from 'firebase-functions';
-import { TriggerAnnotated, Event} from 'firebase-functions';
+import { TriggerAnnotated, Event } from 'firebase-functions';
 import { DeltaSnapshot } from 'firebase-functions/lib/providers/database';
 
+import { setBetaPlan } from './user/add-plan';
 import { MailChimp } from './mailchimp/mailchimp';
 
 let mailchimpConfig = functions.config().mailchimp;
@@ -10,7 +11,10 @@ let mailchimp = new MailChimp(mailchimpConfig.api_key);
 
 export const postUserCreate = functions.database.ref('/users/{id}/common')
   .onCreate(event => {
-    let user = event.data.val()
-    console.log(`Subscribing user to MailChimp`, user);
-    return mailchimp.addUserToList(user, mailchimpConfig.all_users_list_id);
+    let user = event.data.val();
+    console.log(`Subscribing user to MailChimp and adding 'beta' plan`, user);
+    return Promise.all([
+      mailchimp.addUserToList(user, mailchimpConfig.all_users_list_id),
+      setBetaPlan(user.id)
+    ]);
   });
---
+++ firebase/functions/src/user/add-plan.ts
@@ -0,0 +1,6 @@
+import * as admin from 'firebase-admin';
+
+export const setBetaPlan = userId =>
+  admin.database()
+       .ref(`/users/${userId}/plan`)
+       .update({ plan_id: 'beta', created_at: Date.now() });
