3948fade0401bb80a29f7c6ff850e50e9804431e
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-12-11 00:51:32 +0100

    feat(cli): rudimentary login support

--- cli/src/bootstrap.ts
+++ cli/src/bootstrap.ts
@@ -1,14 +1,26 @@
 let program = require('commander');
-let package = require('../package.json');
+let pgk = require('../package.json');
+
+import * as firebase from 'firebase';
+import { environment } from './environments/environment';
+
+firebase.initializeApp(environment.firebaseConfig);
 
 program
-  .version(package.version);
+  .version(pgk.version);
 
 program
   .command('login')
   .description('Login to MachineLabs')
-  .action((env, options) => {
-    console.log('Logging in to MachineLabs...');
+  .option('-g --github-token <value>', 'Login using a GitHub token')
+  .action(cmd => {
+
+    let newCredential = firebase.auth.GithubAuthProvider.credential(cmd.githubToken);
+    firebase
+      .auth()
+      .signInWithCredential(newCredential)
+      .catch((error) => console.log('There was an error during login', error))
+      .then(res => console.log('Successfully logged into MachineLabs'));
   });
 
 program.parse(process.argv);
