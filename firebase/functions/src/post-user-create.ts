3456f3d16bac65af706de3155639ea3bd961da5b
Dominic Elm <dominic@thoughtram.io>
2017-12-18 16:25:05 +0100

    fix(mailchimp): make it fail tolerant if config not given

--- firebase/functions/src/post-user-create.ts
+++ firebase/functions/src/post-user-create.ts
@@ -6,15 +6,20 @@ import { setBetaPlan } from './user/add-plan';
 import { MailChimp } from './mailchimp/mailchimp';
 
 let mailchimpConfig = functions.config().mailchimp;
+let mailchimp;
 
-let mailchimp = new MailChimp(mailchimpConfig.api_key);
+if (mailchimpConfig) {
+  console.warn('MailChimp config not set. For production make sure this is correctly configured.');
+} else {
+  mailchimp = new MailChimp(mailchimpConfig.api_key);
+}
 
 export const postUserCreate = functions.database.ref('/users/{id}/common')
   .onCreate(event => {
     let user = event.data.val();
     console.log(`Subscribing user to MailChimp and adding 'beta' plan`, user);
     return Promise.all([
-      mailchimp.addUserToList(user, mailchimpConfig.all_users_list_id),
+      mailchimp ? mailchimp.addUserToList(user, mailchimpConfig.all_users_list_id) : Promise.resolve(),
       setBetaPlan(user.id)
     ]);
   });
