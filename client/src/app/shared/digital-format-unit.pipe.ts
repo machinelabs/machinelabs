ca229b6a66d07b3ebe677e4c1348534227a51027
Dominic Elm <dominic@thoughtram.io>
2017-12-19 22:26:40 +0100

    fix: digital-format-unit-pipe to not break for bytes equal to zero

--- client/src/app/shared/digital-format-unit.pipe.ts
+++ client/src/app/shared/digital-format-unit.pipe.ts
@@ -6,6 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DigitalFormatUnitPipe implements PipeTransform {
   transform(bytes: number) {
     const units = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB'];
+
+    if (bytes <= 0) {
+      return `0 ${units[0]}`;
+    }
+
     const value = Math.floor(Math.log(bytes) / Math.log(1024));
     return `${(bytes / Math.pow(1024, Math.floor(value))).toFixed(2)} ${units[value]}`;
   }
