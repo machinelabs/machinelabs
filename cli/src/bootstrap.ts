184016335999c81c2b9714542a7d6f3e04ec239d
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2018-01-04 13:36:35 +0100

    feat: rename import to save and allow specifying id

--- cli/src/bootstrap.ts
+++ cli/src/bootstrap.ts
@@ -4,7 +4,7 @@ let pgk = require('../package.json');
 import './firebase/fb';
 
 import './commands/login'
-import './commands/import';
+import './commands/save';
 import './commands/init';
 
 program
