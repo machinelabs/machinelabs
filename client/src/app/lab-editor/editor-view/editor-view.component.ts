machinelabs/machinelabs

commit e7c646d7f7b984e83c60024d376681f308ca3cfe

feat(editor): replace ace editor with monaco editor

client/src/app/lab-editor/editor-view/editor-view.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MatDialog, MatDialogRef, MatSnackBar, MatTabGroup, MatDrawer } from '@angular/material';
 import { FormControl } from '@angular/forms';
+import { MonacoFile } from 'ngx-monaco';
 
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 import { filter, tap, map, skip, switchMap, take, share } from 'rxjs/operators';
 
 import { File, ExecutionRejectionReason } from '@machinelabs/models';
-import { AceEditorComponent } from '../../editor/ace-editor/ace-editor.component';
 import { XtermComponent } from '../../editor/xterm/xterm.component';
 import {
   EditLabDialogComponent,
@@ -30,6 +30,7 @@ import { LocationHelper } from '../../util/location-helper';
 import { Execution } from '../../models/execution';
 import { EditorToolbarAction, EditorToolbarActionTypes } from '../editor-toolbar/editor-toolbar.component';
 import { TimeoutError, RateLimitError } from '../../editor/remote-code-execution/errors';
+import { MonacoFileTypeAdapter } from '../../editor/monaco-file-type-adapter';
 
 const METADATA_SIDEBAR_OPEN_TIMEOUT = 600;
 const INITIAL_LOADING_INDICATOR_PROGRESS = 10;
@@ -97,9 +98,9 @@ export class EditorViewComponent implements OnInit, AfterViewInit {
 
   @ViewChild('outputPanel') outputPanel: XtermComponent;
 
-  @ViewChild('editor') editor: AceEditorComponent;
-
   TabIndex = TabIndex;
+
+  MonacoFileTypeAdapter = MonacoFileTypeAdapter;
 
   pauseModeControl = new FormControl(false);
 
@@ -155,14 +156,7 @@ export class EditorViewComponent implements OnInit, AfterViewInit {
 
   selectTab(tabIndex: TabIndex) {
     this.editorService.selectTab(tabIndex);
-    if (this.editorService.editorTabActive() && this.editor) {
-      // This has to run in the next tick after the editor has become visible
-      // https://github.com/ajaxorg/ace/issues/3070
-      setTimeout(_ => this.editor.resize(), 0);
-      if (!this.fileTreeSidebarOpened) {
-        this.openFileTreeSidebar();
-      }
-    } else if (this.editorService.consoleTabActive() && this.outputPanel) {
+    if (this.editorService.consoleTabActive() && this.outputPanel) {
       setTimeout(_ => this.outputPanel.resize(), 0);
     }
   }
@@ -401,6 +395,10 @@ export class EditorViewComponent implements OnInit, AfterViewInit {
       this.executionMetadataSidebar.close();
       this.editorSnackbar.notifyLabRestored();
     }, METADATA_SIDEBAR_OPEN_TIMEOUT);
+  }
+
+  onFileChange(file: MonacoFile) {
+    this.activeFile = MonacoFileTypeAdapter.monacoFileToLabFile(file);
   }
 
   private goToLab(lab?: Lab, queryParams?) {

client/e2e/editor-view/editor-view.e2e-spec.ts
@@ -19,14 +19,6 @@ describe('Editor-View Component', function() {
     expect(editorView.activeTabLabel).toEqual('Editor');
   });
 
-  it('should show the correct editor theme when switching editor views', () => {
-    expect(editorView.editorMode).toEqual('python');
-
-    editorView.getTabByLabel('Editor').click();
-    expect(editorView.editorMode).toEqual('python');
-  });
-
-
   describe('in EDIT mode', () => {
     it('should display the current lab file structure', () => {
       expect(editorView.fileTree.count()).toEqual(2);

client/e2e/editor-view/editor-view.page-object.ts
@@ -35,10 +35,6 @@ export class EditorViewPageObject {
     return element(by.css('.mat-tab-link[ng-reflect-active="true"]')).getText();
   }
 
-  get editorMode(): promise.Promise<string> {
-    return element(by.tagName('ml-ace-editor.active')).getAttribute('mode');
-  }
-
   getTabByLabel(label: string): ElementArrayFinder {
     return this.tabs.filter((elem, index) => elem.getText().then(text => text === label));
   }

client/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule, PreloadAllModules } from '@angular/router';
 import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { MonacoEditorModule } from 'ngx-monaco';
 
 import { SharedModule } from './shared/shared.module';
 
@@ -51,7 +52,18 @@ const AnimationsModule = [environment.testing ? NoopAnimationsModule : BrowserAn
     FlexLayoutModule,
     RouterModule.forRoot(APP_ROUTES, { preloadingStrategy: PreloadAllModules }),
     SlimLoadingBarModule.forRoot(),
-    SharedModule.forRoot()
+    SharedModule.forRoot(),
+    MonacoEditorModule.forRoot({
+      options: {
+        minimap: {
+          enabled: false
+        },
+        quickSuggestions: true,
+        scrollBeyondLastLine: false,
+        folding: true,
+        fixedOverflowWidgets: true
+      }
+    })
   ],
   providers: [
     LoginService,

client/src/app/editor/ace-editor/ace-editor.component.scss
@@ -1,10 +1,0 @@
-:host {
-  height: 100%;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-
-  font-size: 0.85em;
-}

client/src/app/editor/ace-editor/ace-editor.component.spec.ts
@@ -1,28 +1,0 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
-import { AceEditorComponent } from './ace-editor.component';
-
-describe('AceEditorComponent', () => {
-  let component: AceEditorComponent;
-  let fixture: ComponentFixture<AceEditorComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AceEditorComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AceEditorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});

client/src/app/editor/ace-editor/ace-editor.component.ts
@@ -1,148 +1,0 @@
-import {
-  Component,
-  OnInit,
-  Input,
-  Output,
-  EventEmitter,
-  AfterViewInit,
-  ElementRef,
-  OnChanges
-} from '@angular/core';
-
-declare const ace: any;
-
-const ACE_EDITOR_THEME_PREFIX = 'ace/theme/';
-const ACE_EDITOR_THEME_DEFAULT = `${ACE_EDITOR_THEME_PREFIX}github`;
-const ACE_EDITOR_MODE_PREFIX = 'ace/mode/';
-
-@Component({
-  selector: 'ml-ace-editor',
-  template: `<ng-content></ng-content>`,
-  styleUrls: ['./ace-editor.component.scss']
-})
-export class AceEditorComponent implements AfterViewInit, OnChanges {
-
-  private editor;
-  private _mode;
-  private _theme;
-
-  @Input() value = null;
-
-  @Input() readOnly = false;
-
-  @Input() showGutter = true;
-
-  @Input() hlActiveLine = false;
-
-  @Input() showPrintMargin = false;
-
-  @Input() undo = false;
-
-  @Input() wordWrap = false;
-
-  @Input() set theme(value) {
-    this._theme = ACE_EDITOR_THEME_PREFIX + value;
-  }
-
-  get theme() {
-    return this._theme;
-  }
-
-  @Input() set mode(value) {
-    this._mode = ACE_EDITOR_MODE_PREFIX + value;
-  }
-
-  get mode() {
-    return this._mode;
-  }
-
-  @Output() valueChange = new EventEmitter();
-
-  constructor(private element: ElementRef) {
-
-  }
-
-  ngOnChanges(changes) {
-    // we need to check if editor is defined because ngOnChanges
-    // runs before ngAfterViewInit(). Is there a better way?
-    if (this.editor !== undefined) {
-      if (changes.value
-          && this.value !== null
-          && this.value !== this.editor.getValue()) {
-        this.editor.setValue(this.value, 1);
-      }
-
-      if (changes.readOnly) {
-        this.editor.setReadOnly(this.readOnly);
-      }
-
-      if (changes.mode) {
-        this.editor.getSession().setMode(this.mode);
-      }
-
-      if (changes.theme) {
-        this.editor.setTheme(this.theme);
-      }
-
-      if (changes.showPrintMargin) {
-        this.editor.setShowPrintMargin(this.showPrintMargin);
-      }
-
-      if (changes.hlActiveLine) {
-        this.editor.setHighlightActiveLine(this.hlActiveLine);
-      }
-
-      if (changes.wordWrap) {
-        this.editor.setUseWrapMode(this.wordWrap);
-      }
-    }
-  }
-
-  ngAfterViewInit() {
-    this.editor = ace.edit(this.element.nativeElement);
-
-    if (!this.undo) {
-      this.editor.getSession().setUndoManager(null);
-    }
-
-    this.editor.setTheme(this.theme);
-    this.editor.setReadOnly(this.readOnly);
-    this.editor.setHighlightActiveLine(this.hlActiveLine);
-    this.editor.setShowPrintMargin(this.showPrintMargin);
-
-    this.editor.getSession().setMode(this.mode);
-    this.editor.getSession().setUseWrapMode(this.wordWrap);
-    this.editor.renderer.setShowGutter(this.showGutter);
-
-    // if content children are used `value` might be null
-    if (this.value !== null) {
-      this.editor.setValue(this.value);
-    }
-
-    this.editor.gotoLine(1);
-
-    this.editor.getSession().on('change', (e) => {
-      this.valueChange.emit(this.editor.getValue());
-    });
-  }
-
-  clear() {
-    if (this.editor) {
-      this.editor.setValue('');
-    }
-  }
-
-  resize() {
-    if (this.editor) {
-      this.editor.resize();
-    }
-  }
-
-  scrollToLine(line: number) {
-    this.editor.scrollToLine(line);
-  }
-
-  scrollToBottom() {
-    this.scrollToLine(this.editor.getSession().getValue().length);
-  }
-}

client/src/app/editor/editor.module.ts
@@ -13,7 +13,6 @@ import { EditorLayoutFooterComponent } from './layout/editor-layout-footer.compo
 
 import { FileTreeComponent } from './file-tree/file-tree.component';
 import { NameDialogComponent } from './name-dialog/name-dialog.component';
-import { AceEditorComponent } from './ace-editor/ace-editor.component';
 import { XtermComponent } from './xterm/xterm.component';
 import { FileOutputsComponent } from './file-outputs/file-outputs.component';
 import { FilePreviewDialogToolbarComponent } from './file-preview/file-preview-dialog-toolbar/file-preview-dialog-toolbar.component';
@@ -34,7 +33,6 @@ import { FileTreeService } from './file-tree/file-tree.service';
     ReactiveFormsModule
   ],
   declarations: [
-    AceEditorComponent,
     XtermComponent,
     EditorLayoutComponent,
     EditorLayoutHeaderComponent,
@@ -59,7 +57,6 @@ import { FileTreeService } from './file-tree/file-tree.service';
     FilePreviewDialogService
   ],
   exports: [
-    AceEditorComponent,
     XtermComponent,
     EditorLayoutComponent,
     EditorLayoutHeaderComponent,

commit 9a81cedc04a04bbd7bbdcf544cc8d8148832f5a8

fix(EditorView): don't overwrite activeFile reference on file change
This fixes a regression introduced in https://github.com/machinelabs/machinelabs/commit/e7c646d7f7b984e83c60024d376681f308ca3cfe where we accidently change the
`activeFile` reference with every change that is happening in a file that
is being edited in the editor view.

This caused our app to never propagate changes to the active file back
into the lab that is sent to the server to run it.

client/src/app/lab-editor/editor-view/editor-view.component.ts
@@ -411,7 +411,7 @@ export class EditorViewComponent implements OnInit, AfterViewInit {
   }
 
   onFileChange(file: MonacoFile) {
-    this.activeFile = MonacoFileTypeAdapter.monacoFileToLabFile(file);
+    this.activeFile.content = file.content;
   }
 
   private goToLab(lab?: Lab, queryParams?) {