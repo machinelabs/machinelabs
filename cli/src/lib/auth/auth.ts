b94226fe5fa26cf9691f9fabe9209b6366fd9d89
Dominic Elm <dominic@thoughtram.io>
2018-01-28 18:19:01 +0100

    fix(cli/auth): return error observable in case of auth failure

--- cli/src/lib/auth/auth.ts
+++ cli/src/lib/auth/auth.ts
@@ -1,19 +1,20 @@
 import * as firebase from 'firebase';
+import * as chalk from 'chalk';
 import { Observable } from 'rxjs/Observable';
-import { fromPromise } from 'rxjs/observable/fromPromise';
+import { fromPromise } from 'rxjs/Observable/fromPromise';
+import { catchError } from 'rxjs/operators/catchError';
 import { configstore } from '../../configstore';
 
 export const loginFromCache = () =>
-  fromPromise(
-    firebase
-      .auth()
-      .signInWithCustomToken(configstore.get('lastToken'))
-      .catch(e => console.error('Authentication failed. Try logging in again with `ml login`'))
+  fromPromise(firebase.auth().signInWithCustomToken(configstore.get('lastToken')))
+  .pipe(
+    catchError(error => {
+      console.error(chalk.default.red('Authentication failed. Try logging in again with `ml login`'));
+      return Observable.throw(error);
+    })
   );
-
 
 export const loginAndCache = (customToken: string) => {
   configstore.set('lastToken', customToken);
   return loginFromCache();
 };
-
