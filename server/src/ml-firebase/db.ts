c9ea86a1cfa17f0e2e85b70a96d656677e5b8773
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2018-01-30 12:15:45 +0100

    chore(server): initialize fb with empty cert for testing

--- server/src/ml-firebase/db.ts
+++ server/src/ml-firebase/db.ts
@@ -3,12 +3,22 @@ import { environment } from '../environments/environment';
 import { Config } from '../util/config';
 import { DbRefBuilder } from '@machinelabs/core';
 import { SYSTEM_USER } from '@machinelabs/models';
+import { credential } from 'firebase-admin';
+
+let privateKey = Config.tryGetEnv(Config.ENV_PRIVATE_KEY);
+let clientEmail = Config.tryGetEnv(Config.ENV_CLIENT_EMAIL);
+
+let credentials = { getAccessToken: () => <any>{} };
+
+if (privateKey && clientEmail) {
+  credentials = admin.credential.cert(<any>{
+    'private_key': privateKey,
+    'client_email': clientEmail
+  });
+}
 
 admin.initializeApp({
-  credential: admin.credential.cert(<any>{
-    'private_key': Config.getEnv(Config.ENV_PRIVATE_KEY),
-    'client_email': Config.getEnv(Config.ENV_CLIENT_EMAIL),
-  }),
+  credential: credentials,
   databaseURL: environment.firebaseConfig.databaseURL,
   databaseAuthVariableOverride: {
     uid: SYSTEM_USER
