4abb2d53a09a34a46419715641fc17abba39eb18
Dominic Elm <dominic@thoughtram.io>
2018-02-01 13:19:15 -0500

    feat(cli): add logout command

--- cli/src/lib/auth/auth.ts
+++ cli/src/lib/auth/auth.ts
@@ -1,16 +1,18 @@
 import * as firebase from 'firebase';
 import * as chalk from 'chalk';
 import { Observable } from 'rxjs/Observable';
+import { defer } from 'rxjs/Observable/defer';
+import { _throw } from 'rxjs/Observable/throw';
 import { fromPromise } from 'rxjs/Observable/fromPromise';
 import { catchError } from 'rxjs/operators/catchError';
 import { configstore } from '../../configstore';
 
 export const loginFromCache = () =>
-  fromPromise(firebase.auth().signInWithCustomToken(configstore.get('lastToken')))
+  fromPromise(firebase.auth().signInWithCustomToken(configstore.get('lastToken') || ''))
   .pipe(
     catchError(error => {
       console.error(chalk.default.red('Authentication failed. Try logging in again with `ml login`'));
-      return Observable.throw(error);
+      return _throw(error);
     })
   );
 
@@ -18,3 +20,8 @@ export const loginAndCache = (customToken: string) => {
   configstore.set('lastToken', customToken);
   return loginFromCache();
 };
+
+export const logout = () => defer(() => {
+  configstore.delete('lastToken');
+  return fromPromise(firebase.auth().signOut());
+});
