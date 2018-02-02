717bda148d8c05ce61131e372aa3cb28acc5c734
Sam Verschueren <sam.verschueren@gmail.com>
2018-02-02 07:45:55 +0100

    refactor(cli): Use RxJS pipeable operators

--- cli/src/lib/auth/auth.ts
+++ cli/src/lib/auth/auth.ts
@@ -1,13 +1,15 @@
 import * as firebase from 'firebase';
-import { Observable } from '@reactivex/rxjs';
+import { Observable } from 'rxjs/Observable';
+import { fromPromise } from 'rxjs/observable/fromPromise';
 import { configstore } from '../../configstore';
 
 export const loginFromCache = () =>
-  Observable.fromPromise(
+  fromPromise(
     firebase
       .auth()
       .signInWithCustomToken(configstore.get('lastToken'))
-      .catch(e => console.error('Authentication failed. Try logging in again with `ml login`')));
+      .catch(e => console.error('Authentication failed. Try logging in again with `ml login`'))
+  );
 
 
 export const loginAndCache = (customToken: string) => {
