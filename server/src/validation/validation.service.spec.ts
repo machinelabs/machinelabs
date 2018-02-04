5186b2bbb0af0cd45b278db4890d32213395cb00
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2018-02-04 13:21:57 +0100

    test(server): prove that resolved vals are cached

--- server/src/validation/validation.service.spec.ts
+++ server/src/validation/validation.service.spec.ts
@@ -4,8 +4,12 @@ import { ValidationService } from './validation.service';
 import { ValidationRule } from './rules/rule';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
-import { delay } from 'rxjs/operators';
+import { delay, map } from 'rxjs/operators';
 import { Invocation, InvocationType, ExecutionRejectionInfo, ExecutionRejectionReason } from '@machinelabs/models';
+import { Resolver } from 'src/validation/resolver/resolver';
+import isBoolean = require('lodash.isboolean');
+import { ValidationError } from 'src/validation/validation-result';
+
 
 let dummyInvocation: Invocation = {
   id: 'dummy',
@@ -35,7 +39,59 @@ class ApprovedRule implements ValidationRule {
   }
 }
 
+class TrueResolver implements Resolver {
+
+  calls = 0;
+
+  resolve(invocation: Invocation): Observable<boolean> {
+    this.calls++;
+
+    return of(true);
+  }
+}
+
+class IsTrueRule implements ValidationRule {
+  check(validationContext: Invocation, resolves: Map<Function, Observable<any>>): Observable<boolean | ValidationError> {
+    return resolves.get(TrueResolver)
+                   .pipe(map(val => {
+                      if (!val) {
+                        return new ExecutionRejectionInfo(null, 'must be true');
+                      }
+                      return true;
+                   }));
+  }
+}
+
+class IsBooleanRule implements ValidationRule {
+  check(validationContext: Invocation, resolves: Map<Function, Observable<any>>): Observable<boolean | ValidationError> {
+    return resolves.get(TrueResolver)
+                   .pipe(map(val => {
+                      if (!isBoolean(val)) {
+                        return new ExecutionRejectionInfo(null, 'must be boolean');
+                      }
+                      return true;
+                    }));
+  }
+}
+
 describe('.validate()', () => {
+
+  it('should cache resolves', () => {
+    expect.assertions(2);
+    let svc = new ValidationService();
+    let dummyResolver = new TrueResolver();
+    svc.addResolver(TrueResolver, dummyResolver)
+       .addRule(new IsTrueRule())
+       .addRule(new IsBooleanRule());
+
+
+    svc.validate(dummyInvocation)
+       .subscribe(validationContext => {
+         expect(validationContext.validationResult).toBeTruthy();
+         expect(dummyResolver.calls).toBe(1);
+      });
+  });
+
   it('should return ExecutionRejectionInfo if at least one rule fails', () => {
     expect.assertions(1);
     let svc = new ValidationService();
