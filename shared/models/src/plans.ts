37cc4e9d7efb8ffe9e5051be281c95c60cfb2eea
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2018-02-04 18:55:31 +0100

    feat(@machinelabs/models): expose maxFileUploads on plan

--- shared/models/src/plans.ts
+++ shared/models/src/plans.ts
@@ -1,6 +1,7 @@
 export interface Credits {
   gpuHoursPerMonth: number;
   cpuHoursPerMonth: number;
+  maxFileUploads: number;
 }
 
 export enum PlanId {
@@ -11,8 +12,8 @@ export enum PlanId {
 }
 
 export const PlanCredits = new Map<string, Credits>([
-  [PlanId.None, { gpuHoursPerMonth: 0, cpuHoursPerMonth: 0 }],
-  [PlanId.Admin, { gpuHoursPerMonth: Number.POSITIVE_INFINITY, cpuHoursPerMonth: Number.POSITIVE_INFINITY }],
-  [PlanId.Beta, { gpuHoursPerMonth: 0, cpuHoursPerMonth: 72 }],
-  [PlanId.BetaBacker, { gpuHoursPerMonth: 20, cpuHoursPerMonth: 72 }]
+  [PlanId.None, { gpuHoursPerMonth: 0, cpuHoursPerMonth: 0, maxFileUploads: 0 }],
+  [PlanId.Admin, { gpuHoursPerMonth: Number.POSITIVE_INFINITY, cpuHoursPerMonth: Number.POSITIVE_INFINITY, maxFileUploads: 20 }],
+  [PlanId.Beta, { gpuHoursPerMonth: 0, cpuHoursPerMonth: 72, maxFileUploads: 5 }],
+  [PlanId.BetaBacker, { gpuHoursPerMonth: 20, cpuHoursPerMonth: 72, maxFileUploads: 10 }]
 ]);
