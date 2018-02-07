1517f9a26f2cb4a54b6e13b68a9979ac2ba4031d
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2018-02-07 13:01:51 +0100

    fix(cli): repair broken init command

--- cli/src/commands/init.ts
+++ cli/src/commands/init.ts
@@ -1,20 +1,20 @@
 let program = require('commander');
 import * as chalk from 'chalk';
-import { writeFileSync } from 'fs';
-import { readLabDirectory, getMlYamlFromPath, ML_YAML } from '@machinelabs/core';
+import { writeFileSync, existsSync } from 'fs';
+import { readLabDirectory, getMlYamlFromPath, ML_YAML, ML_YAML_FILENAME } from '@machinelabs/core';
 
 program
 .command('init')
 .description('Initialize current directory as lab')
 .option('-f --force', 'Overwrite ml.yaml if it already exists')
 .action(cmd => {
-  let config = getMlYamlFromPath('.');
+  let configExists = existsSync(ML_YAML_FILENAME);
 
-  if (config && !cmd.force) {
+  if (configExists && !cmd.force) {
     console.error(chalk.default.red('ml.yaml already exists. Run `ml init --force` if you intend to overwrite ml.yaml with default settings'));
     process.exit(1);
   }
 
-  writeFileSync('ml.yaml', ML_YAML);
+  writeFileSync(ML_YAML_FILENAME, ML_YAML);
   process.exit();
 });
