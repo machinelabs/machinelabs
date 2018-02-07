8b4ba753fe00fca1f40058c011bc86009a4b5fb8
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2018-02-07 14:42:10 +0100

    feat(cli): show help and exit on invalid cmds

---
+++ cli/src/commands/catch-all.ts
@@ -0,0 +1,12 @@
+import program = require('commander');
+import * as chalk from 'chalk';
+import { writeFileSync, existsSync } from 'fs';
+import { readLabDirectory, getMlYamlFromPath, ML_YAML, ML_YAML_FILENAME } from '@machinelabs/core';
+
+program
+  .command('*')
+  .action(() => {
+    console.log(chalk.default.yellow(`Invalid command. Here's some help to get you started`));
+    program.outputHelp();
+    process.exit(1);
+  });
