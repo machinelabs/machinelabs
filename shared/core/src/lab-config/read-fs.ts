6d317c88fa6c4a051b7d177f2e6e1d21eb06864e
Dominic Elm <dominic@thoughtram.io>
2018-02-09 15:01:57 +0100

    feat(shared/core): catch exception when reading lab config from disk

--- shared/core/src/lab-config/read-fs.ts
+++ shared/core/src/lab-config/read-fs.ts
@@ -3,10 +3,14 @@ import { join } from 'path';
 import { ML_YAML_FILENAME } from './ml.yaml';
 
 export const getMlYamlFromPath = (path: string) => {
+  try {
+    let content = readFileSync(join(path, ML_YAML_FILENAME)).toString();
 
-  let content = readFileSync(join(path, ML_YAML_FILENAME)).toString();
-  return {
-    content: content,
-    name: ML_YAML_FILENAME
-  };
+    return {
+      name: ML_YAML_FILENAME,
+      content
+    };
+  } catch (error) {
+    return null;
+  }
 };
