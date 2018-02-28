d489276dda7512b9922ee699ac4f206cdeaca53d
Pascal Precht <pascal.precht@googlemail.com>
2018-02-28 16:38:33 +0100

    refactor: refactor handshake terminal ui into reusable component

---
+++ client/src/app/shared/console/console.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ml-console',
+  templateUrl: './console.component.html',
+  styleUrls: ['./console.component.scss']
+})
+export class ConsoleComponent {
+
+}
