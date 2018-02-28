40aacf7fde49d5a17e53ff02cd9eb0e3a1ff350c
Pascal Precht <pascal.precht@googlemail.com>
2018-02-28 16:24:43 +0100

    uiux: overhaul page-footer component

--- client/src/app/shared/page-footer/page-footer.component.ts
+++ client/src/app/shared/page-footer/page-footer.component.ts
@@ -2,31 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ml-page-footer',
-  template: `
-    <footer>
-      <p>&copy; 2017 MachineLabs Inc. All rights reserved.</p>
-      <p><a href="/terms-of-service" title="Terms of Service">Terms of Service</a></p>
-    </footer>
-  `,
-  styles: [`
-    :host {
-      display: block;
-      text-align: center;
-      font-size: 0.9em;
-      padding: 1rem;
-      background: rgb(59, 59, 68);
-      color: #fff;
-    }
-
-    a {
-      color: #fff;
-    }
-
-    footer {
-      max-width: 1200px;
-      margin: 0 auto;
-      padding: 0 20px;
-    }
-  `]
+  templateUrl: './page-footer.component.html',
+  styleUrls: ['./page-footer.component.scss']
 })
 export class PageFooterComponent {}
