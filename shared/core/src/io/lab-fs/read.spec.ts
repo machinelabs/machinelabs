a614d23b1aef82bebf4a8d3631978d25ece8c091
Sam Verschueren <sam.verschueren@gmail.com>
2018-03-12 17:26:03 +0100

    feat(cli): Support globbing patterns
    fixes #751

---
+++ shared/core/src/io/lab-fs/read.spec.ts
@@ -0,0 +1,91 @@
+import 'jest';
+import * as rimraf from 'rimraf';
+
+import { writeDirectory } from './fs';
+import { readLabDirectory } from './read';
+
+describe('readLabDirectory', () => {
+
+  beforeAll(() => {
+    let dir = {
+        name: 'foo',
+        contents: [
+          {
+            name: 'foo.txt',
+            content: 'foo'
+          },
+          {
+            name: 'bar',
+            contents: [
+              {
+                name: 'bar.js',
+                content: 'bar'
+              },
+              {
+                name: '.foo',
+                content: 'foo dotfile'
+              },
+              {
+                name: 'readme.txt',
+                content: 'Readme file'
+              }
+            ]
+          }
+        ]
+      };
+
+      writeDirectory(dir).subscribe();
+  });
+
+  afterAll(() => {
+    rimraf.sync('foo');
+  });
+
+  it('should read entire directory', () => {
+    const result = readLabDirectory('foo');
+
+    expect(result).toEqual([
+      {
+        name: 'foo.txt',
+        content: 'foo'
+      },
+      {
+        name: 'bar',
+        contents: [
+          {
+            name: '.foo',
+            content: 'foo dotfile'
+          },
+          {
+            name: 'bar.js',
+            content: 'bar'
+          },
+          {
+            name: 'readme.txt',
+            content: 'Readme file'
+          }
+        ]
+      }
+    ]);
+  });
+
+  it('should exclude globbing patterns files', () => {
+    const result = readLabDirectory('foo', {exclude: ['**/*.txt']});
+
+    expect(result).toEqual([
+      {
+        name: 'bar',
+        contents: [
+          {
+            name: '.foo',
+            content: 'foo dotfile'
+          },
+          {
+            name: 'bar.js',
+            content: 'bar'
+          }
+        ]
+      }
+    ]);
+  });
+});
--- shared/core/src/io/lab-fs/read.ts
+++ shared/core/src/io/lab-fs/read.ts
@@ -1,90 +1,106 @@
 import { readdirSync, statSync, readFileSync } from 'fs';
-import { basename, extname, join } from 'path';
+import { basename, extname, join, dirname } from 'path';
+import * as globby from 'globby';
 
 import { File, Directory, LabDirectory, instanceOfDirectory } from '@machinelabs/models';
 
-const matcher = require('matcher');
 const isBinaryFile = require('isbinaryfile').sync;
 
 
 export interface ReadOptions {
-  extensions: RegExp;
-  exclude: RegExp;
-  excludeBinaries: boolean;
+  extensions?: RegExp;
+  exclude?: string[];
+  excludeBinaries?: boolean;
 }
 
-const safeReadDirSync = (path: string) => {
-  let dirData = [];
-  try {
-    dirData = readdirSync(path);
-  } catch (ex) {
-    if (ex.code == 'EACCES') {
-      // User does not have permissions, ignore directory
-      return null;
-    }
-
-    throw ex;
+const createDirectoryStructure = (path: string, directories: Map<string, Directory>) => {
+  if (directories.has(path)) {
+    // Early exit if the directory is already present
+    return;
   }
-  return dirData;
+
+  const dirs = path.split('/');
+  let result: Directory;
+
+  while (dirs.length > 0) {
+    const dirPath = join(...dirs);
+    const dir = dirs.pop();
+
+    const directory = directories.get(dirPath);
+
+    if (directory) {
+      directory.contents.push(result);
+
+      return;
+    } else {
+      result = {
+        name: dir,
+        contents: result ? [result] : []
+      };
+
+      directories.set(dirPath, result);
+    }
+  }
+
+  return result;
 };
 
-export const readDirectory = (path: string, options?: ReadOptions): Directory|File => {
-  const name = basename(path);
-  let stats;
+export const readDirectory = (path: string, options: ReadOptions = {}): Directory => {
+  const excludePatterns = Array.isArray(options.exclude) ? options.exclude : [];
 
-  try {
-    stats = statSync(path);
-  } catch (e) {
-    return null;
-  }
+  const files = globby.sync(['**/*'], {
+    cwd: path,
+    ignore: excludePatterns,
+    dot: true
+  });
 
-  // Skip if it matches the exclude regex
-  if (path !== '.' &&
-      options &&
-      Array.isArray(options.exclude) &&
-      options.exclude.length &&
-      matcher([path], options.exclude).length) {
-    return null;
-  }
+  const result: (Directory | File)[] = [];
+  const directories = new Map<string, Directory>();
 
-  if (stats.isFile()) {
+  for (const file of files) {
+    // Extract the filename, extension and directory name out of the path
+    const filename = basename(file);
+    const extension = extname(filename).toLowerCase();
+    const directoryName = dirname(file);
 
-    const ext = extname(path).toLowerCase();
-
-    let item: File = {
-      name: name,
-      content: readFileSync(path).toString()
-    };
-
-    // Skip if it does not match the extension regex
+    // Skip if it does not match the extension regex or it is a binary file
     if (options &&
-      (options.extensions && !options.extensions.test(ext) ||
+      (options.extensions && !options.extensions.test(extension) ||
       (options.excludeBinaries && isBinaryFile(path)))) {
       return null;
     }
 
-    return item;
-  } else if (stats.isDirectory()) {
+    if (directoryName === '.') {
+      // If the directory is the root directory, just push the file into the result
+      result.push({
+        name: filename,
+        content: readFileSync(join(path, file)).toString()
+      });
+    } else {
+      // Create the entire directory structure and keep track of individual directories
+      const structure = createDirectoryStructure(directoryName, directories);
 
-    let dirData = safeReadDirSync(path);
-    if (dirData === null) {
-      return null;
+      if (structure) {
+        // If a structure is returned, it is a new structure and we should add it to the result
+        result.push(structure);
+      }
+
+      // Push the file into the correct directory
+      const directory = directories.get(directoryName);
+      directory.contents.push({
+        name: filename,
+        content: readFileSync(join(path, file)).toString()
+      });
     }
-
-    let contents = dirData
-      .map(child => readDirectory(join(path, child), options))
-      .filter(val => !!val);
-
-    return {
-      name: name,
-      contents: contents
-    };
   }
 
-  return null;
+  return {
+    name: '.',
+    contents: result
+  };
 };
 
-export const readLabDirectory = (path: string, options?: any): LabDirectory => {
+export const readLabDirectory = (path: string, options?: ReadOptions): LabDirectory => {
   let dir = readDirectory(path, options);
 
   if (instanceOfDirectory(dir)) {
