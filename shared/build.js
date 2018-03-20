4429c5212a26b1a324f20f67b228720e700526f6
Dominic Elm <dominic@thoughtram.io>
2018-03-20 12:59:22 +0100

    style(shared): format build script

--- shared/build.js
+++ shared/build.js
@@ -4,17 +4,22 @@
 // that the admin-cli has a `build-shared` command that basically
 // just invokes this script as a convenience command.
 
-const execute = require('child_process').execSync;
+const execute = require("child_process").execSync;
 
-const doInstall = !!process.argv.includes('--install');
+const doInstall = !!process.argv.includes("--install");
 
 // packages need to be built in order (e.g core depends on models)
-let buildOrder = ['models', 'core', 'metrics', 'supervisor'];
+let buildOrder = ["models", "core", "metrics", "supervisor"];
 
-const buildCmd = dir => execute(`(cd ${dir} && ./node_modules/typescript/bin/tsc)`, { stdio: 'inherit' });
+const buildCmd = dir =>
+  execute(`(cd ${dir} && ./node_modules/typescript/bin/tsc)`, {
+    stdio: "inherit"
+  });
 
-const installAndBuildCmd = dir => execute(`(cd ${dir} && yarn install && ./node_modules/typescript/bin/tsc)`, { stdio: 'inherit' });
+const installAndBuildCmd = dir =>
+  execute(`(cd ${dir} && yarn install && ./node_modules/typescript/bin/tsc)`, {
+    stdio: "inherit"
+  });
 
-console.log('Building shared libs...');
+console.log("Building shared libs...");
 buildOrder.forEach(doInstall ? installAndBuildCmd : buildCmd);
-
