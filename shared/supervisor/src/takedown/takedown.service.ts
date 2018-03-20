356849d28b86da8c7dc7c9b608855724a772d8fb
Dominic Elm <dominic@thoughtram.io>
2018-03-20 13:25:37 +0100

    style(shared/supervisor): update tslint rules and add prettier

--- shared/supervisor/src/takedown/takedown.service.ts
+++ shared/supervisor/src/takedown/takedown.service.ts
@@ -8,11 +8,10 @@ import { filter } from 'rxjs/operators/filter';
 import { tap } from 'rxjs/operators/tap';
 
 export class TakedownService {
-  constructor(private dbRefBuilder: DbRefBuilder) { }
+  constructor(private dbRefBuilder: DbRefBuilder) {}
 
   takedown(executionId: string) {
-
-    let id = uniqueId();
+    const id = uniqueId();
     return this.dbRefBuilder
       .invocationRef(id)
       .set({
@@ -22,33 +21,27 @@ export class TakedownService {
         user_id: SYSTEM_USER,
         timestamp: firebase.database.ServerValue.TIMESTAMP
       })
-      .pipe(
-        map(_ => executionId)
-      );
+      .pipe(map(_ => executionId));
   }
 
   takedownByUser(userId: string, hardwareTypes = [HardwareType.CPU, HardwareType.GPU]) {
-    let executionIds$ = this.dbRefBuilder
+    const executionIds$ = this.dbRefBuilder
       .userExecutionsLiveRef(userId)
       .onceValue()
-      .pipe(
-        map(snapshot => snapshot.val()),
-        mergeMap(val => Object.keys(val))
-      );
+      .pipe(map(snapshot => snapshot.val()), mergeMap(val => Object.keys(val)));
 
     if (hardwareTypes.length === Object.keys(HardwareType).length) {
       // no need to fetch executions if we don't have to check the hardware type
       return executionIds$.pipe(tap(id => this.takedown(id)));
     } else {
-      return executionIds$
-        .pipe(
-          mergeMap(id => this.dbRefBuilder.executionRef(id).onceValue()),
-          map(snapshot => snapshot.val()),
-          filter(val => !!val),
-          filter(execution => !!hardwareTypes.find(val => execution.hardware_type)),
-          map(execution => execution.id),
-          tap(id => this.takedown(id))
-        );
+      return executionIds$.pipe(
+        mergeMap(id => this.dbRefBuilder.executionRef(id).onceValue()),
+        map(snapshot => snapshot.val()),
+        filter(val => !!val),
+        filter(execution => !!hardwareTypes.find(val => execution.hardware_type)),
+        map(execution => execution.id),
+        tap(id => this.takedown(id))
+      );
     }
   }
 }
