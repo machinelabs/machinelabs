9ec7a0fd4676dd4dea54818c0e53a9c681ae3344
Dominic Elm <dominic@thoughtram.io>
2018-03-20 13:11:38 +0100

    style(shared/metrics): update tslint rules and add prettier

--- shared/metrics/src/costs/cost-calculator.ts
+++ shared/metrics/src/costs/cost-calculator.ts
@@ -6,31 +6,29 @@ import { CostReport } from './cost-report';
 
 export class CostCalculator {
   public calc(executions: Observable<Execution>): Observable<CostReport> {
-    return executions
-            .pipe(
-              scan((acc: CostReport, execution: Execution) => {
+    return executions.pipe(
+      scan((acc: CostReport, execution: Execution) => {
+        const timeSpent = this.msToSecond((execution.finished_at || Date.now()) - execution.started_at);
 
-                let timeSpent = this.msToSecond((execution.finished_at || Date.now()) - execution.started_at);
+        // to make all past calculations work
+        const hardwareType = <HardwareType>execution.hardware_type.replace('economy', 'cpu');
 
-                // to make all past calculations work
-                let hardwareType = <HardwareType>execution.hardware_type.replace('economy', 'cpu');
+        const secondsPerHardware = acc.secondsPerHardware.get(hardwareType) + timeSpent;
+        acc.secondsPerHardware.set(hardwareType, secondsPerHardware);
 
-                let secondsPerHardware = acc.secondsPerHardware.get(hardwareType) + timeSpent;
-                acc.secondsPerHardware.set(hardwareType, secondsPerHardware);
+        const costsPerHardware = +(secondsPerHardware * COST_PER_SECOND_PER_TYPE.get(hardwareType)).toFixed(2);
+        acc.costPerHardware.set(hardwareType, costsPerHardware);
 
-                let costsPerHardware = +(secondsPerHardware * COST_PER_SECOND_PER_TYPE.get(hardwareType)).toFixed(2);
-                acc.costPerHardware.set(hardwareType, costsPerHardware);
+        acc.totalSeconds = acc.totalSeconds + timeSpent;
 
-                acc.totalSeconds = acc.totalSeconds + timeSpent;
+        acc.totalCost = Array.from(acc.costPerHardware)
+          .map(([key, val]) => val)
+          .reduce((prev, current) => prev + current, 0);
 
-                acc.totalCost = Array.from(acc.costPerHardware)
-                                    .map(([key, val]) => val)
-                                    .reduce((prev, current) => prev + current, 0);
-
-                return acc;
-              }, new CostReport()),
-              last(null, null, new CostReport())
-            );
+        return acc;
+      }, new CostReport()),
+      last(null, null, new CostReport())
+    );
   }
 
   private msToSecond(ms: number) {
--- shared/metrics/src/live/live-metrics.service.ts
+++ shared/metrics/src/live/live-metrics.service.ts
@@ -2,15 +2,12 @@ import { ObservableDbRef, DbRefBuilder } from '@machinelabs/core';
 import { Observable } from 'rxjs/Observable';
 
 export class LiveMetricsService {
-
-  constructor(private db: DbRefBuilder) {
-
-  }
+  constructor(private db: DbRefBuilder) {}
 
   getLiveExecutionsRef() {
-    return this.db.userExecutions()
-                  .orderByChild('live')
-                  .startAt('');
+    return this.db
+      .userExecutions()
+      .orderByChild('live')
+      .startAt('');
   }
-
 }
