423e563770ed9980c044a84a3937d1f6b097ff29
Dominic Elm <dominic@thoughtram.io>
2018-03-20 11:44:08 +0100

    style(rest-api): update tslint rules and add prettier

--- rest-api/build/build.js
+++ rest-api/build/build.js
@@ -1,5 +1,5 @@
 const executeFactory = require('./lib/execute');
-const execute = executeFactory({displayErrors: true});
+const execute = executeFactory({ displayErrors: true });
 const copyConfig = require('./tasks/copy-config');
 const execSync = require('child_process').execSync;
 
@@ -7,11 +7,10 @@ const setRootDir = () => {
   process.chdir(__dirname);
   // We are going one directory up to be in ./rest-api
   process.chdir('../');
-}
+};
 
 setRootDir();
 execute('rm -rf ./dist');
 copyConfig();
 
 execute('./node_modules/typescript/bin/tsc');
-
