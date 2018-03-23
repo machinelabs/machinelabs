9c16e1af6f1842ea75a19ff839cda3d8fd60b758
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2018-03-23 12:59:10 +0100

    fix(admin-cli): really put changelog in release commit
    The previous fix, didn't actually fix the error.

--- admin-cli/lib/generate-changelog.ts
+++ admin-cli/lib/generate-changelog.ts
@@ -4,7 +4,7 @@ import * as chalk from 'chalk';
 import * as fs from 'fs';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
-import { tap, finalize, catchError } from 'rxjs/operators';
+import { tap, catchError, scan, last } from 'rxjs/operators';
 import { conventionalChangelogObservable } from './conventional-changelog-observable';
 
 function createIfMissing(args) {
@@ -30,17 +30,15 @@ export function generateChangelog(args) {
   createIfMissing(options);
 
   let oldContent = options.dryRun ? '' : fs.readFileSync(options.inFile, 'utf8');
-  let newContent = '';
 
   if (oldContent.indexOf('<a name=') !== -1) {
     oldContent = oldContent.substring(oldContent.indexOf('<a name='));
   }
 
   return conventionalChangelogObservable(options).pipe(
-    tap(buffer => {
-      newContent += buffer.toString();
-    }),
-    finalize(() => {
+    scan((prev, curr) => (prev += curr.toString()), ''),
+    last(),
+    tap(newContent => {
       if (options.dryRun) {
         console.info(`\n${chalk.gray(newContent.trim())}\n`);
       } else {
