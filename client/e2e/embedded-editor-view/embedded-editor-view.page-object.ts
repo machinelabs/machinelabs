dd4ed584b9dbaeb65402a650d9bde414afd4124c
Ruben Vermeulen <hello@rubenvermeulen.be>
2018-05-07 22:28:23 +0200

    refactor(e2e): remove protractor from client
    Closes #806

--- client/e2e/embedded-editor-view/embedded-editor-view.page-object.ts
+++
@@ -1,7 +0,0 @@
-import { protractor, browser, element, by, ElementFinder, ElementArrayFinder, promise } from 'protractor';
-
-export class EmbeddedEditorViewPageObject {
-  get activeTabLabel(): promise.Promise<string> {
-    return element(by.css('.mat-tab-link[ng-reflect-active="true"]')).getText();
-  }
-}
