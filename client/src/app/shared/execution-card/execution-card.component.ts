ffe90fdc9e6c6b63bf017ea20c983419887362b4
Jeffrey Bosch <jefiozie.bosch@gmail.com>
2018-08-08 20:23:18 +0200

    feat(client):added has output icon for lab-card
    Closes: #811

--- client/src/app/shared/execution-card/execution-card.component.ts
+++ client/src/app/shared/execution-card/execution-card.component.ts
@@ -1,17 +1,26 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 import { Execution } from '../../models/execution';
 import { ExecutionStatus } from '@machinelabs/models';
+import { OutputFilesService } from '../../output-files.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'ml-execution-card',
   templateUrl: './execution-card.component.html',
   styleUrls: ['./execution-card.component.scss']
 })
-export class ExecutionCardComponent {
+export class ExecutionCardComponent implements OnChanges {
+  hasOutput$: Observable<boolean>;
+
+  constructor(public outputFilesService: OutputFilesService) {}
+
   @Input() execution;
 
   @Output() view = new EventEmitter<Execution>();
-
   ExecutionStatus = ExecutionStatus;
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.execution && changes.execution.currentValue !== null) {
+      this.hasOutput$ = this.outputFilesService.hasOutputFiles(this.execution.id);
+    }
+  }
 }
